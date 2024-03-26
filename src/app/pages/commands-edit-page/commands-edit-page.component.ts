d994b8d6b6beed1f2405e954881db969f74514ea
nook24 <infonook24.eu>
2024-03-26 12:13:50 +0100

    Add test

---
+++ src/app/pages/commands-edit-page/commands-edit-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'oitc-commands-edit-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './commands-edit-page.component.html',
+  styleUrl: './commands-edit-page.component.css'
+})
+export class CommandsEditPageComponent {
+
+}
