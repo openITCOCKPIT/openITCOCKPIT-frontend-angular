001950496db5549460dc1a87bcf5ee3835710e8e
root <root@jgleim-angular-master.itsm.love>
2024-03-26 12:18:14 +0100

    Use implicit calls for icons (global setup) instead of explicit imports.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,4 @@
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faSignOut, faRetweet} from '@fortawesome/free-solid-svg-icons';
 import {Component, inject} from '@angular/core';
 import {AuthService} from "../../auth/auth.service";
 
@@ -11,8 +10,6 @@ import {AuthService} from "../../auth/auth.service";
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  faSignOut = faSignOut;
-  faRetweet = faRetweet;
   private readonly authService = inject(AuthService);
 
   public logout(): void {
