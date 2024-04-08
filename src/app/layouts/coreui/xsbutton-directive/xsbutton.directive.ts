fc6d321d1395ac6430231068adc4dfbca9cb9024
nook24 <info@nook24.eu>
2024-04-08 10:38:51 +0200

    Overwrite cButton instead of copy&paste

--- src/app/layouts/coreui/xsbutton-directive/xsbutton.directive.ts
+++ src/app/layouts/coreui/xsbutton-directive/xsbutton.directive.ts
@@ -1,93 +1,19 @@
-// MIT license
-// (c) CoreUI for Angular and Contributors
-//
-// This is a copy&paste of https://github.com/coreui/coreui-angular/blob/main/projects/coreui-angular/src/lib/button/button.directive.ts
-// But it adds 'xs' option to size.
-// I did not find a solution with extend to overwrite the value
+// Overwrite cButton and add 'xs' option
 
-import { booleanAttribute, Directive, HostBinding, Input } from '@angular/core';
-import { ButtonType, Colors, Shapes } from '@coreui/angular';
+import { Directive, Input } from '@angular/core';
+import { ButtonDirective } from '@coreui/angular';
 
 @Directive({
   selector: '[cButton]',
   exportAs: 'cButton',
   standalone: true
 })
-export class XsButtonDirective {
+export class XsButtonDirective extends ButtonDirective {
+  // Override the size property defined in the original ButtonDirective
+  // @ts-ignore
+  @Input() override size?: 'xs' | 'sm' | 'lg' | '';
 
-  /**
-   * Toggle the active state for the component. [docs]
-   * @type boolean
-   */
-  @Input({ transform: booleanAttribute }) active: string | boolean = false;
-
-  /**
-   * Sets the color context of the component to one of CoreUI’s themed colors. [docs]
-   * @type Colors
-   */
-  @Input() color?: Colors = 'primary';
-  /**
-   * Toggle the disabled state for the component.
-   * @type boolean
-   */
-  @Input({ transform: booleanAttribute }) disabled: string | boolean = false;
-
-  /**
-   * Select the shape of the component.
-   * @type { 'rounded' | 'rounded-top' | 'rounded-end' | 'rounded-bottom' | 'rounded-start' | 'rounded-circle' | 'rounded-pill' | 'rounded-0' | 'rounded-1' | 'rounded-2' | 'rounded-3' | string }
-   */
-  @Input() shape?: Shapes;
-
-  /**
-   * Size the component small or large.
-   * @type {'xs' | 'sm' | 'lg'}
-   */
-  @Input() size?: 'xs' | 'sm' | 'lg' | '' = '';
-
-  /**
-   * Specifies the type of button. Always specify the type attribute for the `<button>` element.
-   * Different browsers may use different default types for the `<button>` element.
-   */
-  @HostBinding('attr.type')
-  @Input() type: ButtonType = 'button';
-
-  /**
-   * Set the button variant to an outlined button or a ghost button.
-   * @type {'ghost' | 'outline'}
-   */
-  @Input() variant?: 'ghost' | 'outline';
-
-  @HostBinding('class')
-  get hostClasses(): any {
-    return {
-      btn: true,
-      [`btn-${this.color}`]: !!this.color && !this.variant,
-      [`btn-${this.variant}`]: !!this.variant && !this.color,
-      [`btn-${this.variant}-${this.color}`]: !!this.variant && !!this.color,
-      [`btn-${this.size}`]: !!this.size,
-      [`${this.shape}`]: !!this.shape,
-      disabled: this.disabled,
-      active: this.active
-    };
-  }
-
-  @HostBinding('attr.aria-disabled')
-  get ariaDisabled() {
-    return this.disabled || null;
-  };
-
-  @HostBinding('attr.aria-pressed')
-  get isActive(): boolean | null {
-    return <boolean>this.active || null;
-  }
-
-  @HostBinding('attr.disabled')
-  get attrDisabled() {
-    return this.disabled ? '' : null;
-  };
-
-  @HostBinding('attr.tabindex')
-  get tabIndex(): string | null {
-    return this.disabled ? '-1' : null;
+  constructor() {
+    super(); // Call the constructor of the base class
   }
 }
