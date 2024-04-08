19cafca709b4cd812f49a205afd8674c2fec6c66
ibering <irina.bering@it-novum.com>
2024-04-08 16:12:45 +0200

    commands index: first steps

--- src/app/pages/commands/commands-index/commands-index.component.ts
+++ src/app/pages/commands/commands-index/commands-index.component.ts
@@ -1,17 +1,43 @@
 import { Component } from '@angular/core';
 import { CoreuiComponent } from '../../../layouts/coreui/coreui.component';
 import { TranslocoDirective } from '@jsverse/transloco';
+import {
+  CardBodyComponent,
+  CardComponent,
+  CardFooterComponent,
+  CardHeaderComponent,
+  CardSubtitleDirective, CardTitleDirective, ListGroupDirective, ListGroupItemDirective, NavComponent, NavItemComponent
+} from "@coreui/angular";
+import {XsButtonDirective} from "../../../layouts/coreui/xsbutton-directive/xsbutton.directive";
+import {faArrowsRotate, faCircleInfo, faFilter, faPlus} from "@fortawesome/free-solid-svg-icons";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'oitc-commands-index',
   standalone: true,
   imports: [
     CoreuiComponent,
-    TranslocoDirective
+    TranslocoDirective,
+    CardBodyComponent,
+    CardComponent,
+    CardFooterComponent,
+    CardHeaderComponent,
+    CardSubtitleDirective,
+    CardTitleDirective,
+    ListGroupDirective,
+    ListGroupItemDirective,
+    NavComponent,
+    NavItemComponent,
+    XsButtonDirective,
+    FaIconComponent
   ],
   templateUrl: './commands-index.component.html',
   styleUrl: './commands-index.component.css'
 })
 export class CommandsIndexComponent {
 
+  protected readonly faCircleInfo = faCircleInfo;
+  protected readonly faArrowsRotate = faArrowsRotate;
+  protected readonly faPlus = faPlus;
+  protected readonly faFilter = faFilter;
 }
