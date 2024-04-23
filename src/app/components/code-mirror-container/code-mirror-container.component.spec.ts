c7f143d997f92119ac9f3254f040d6dab5ace722
abergen <alex.bergen@it-novum.com>
2024-04-23 07:16:21 +0000

    - added codemirror container component for textareas for highlighting and autocompletion

---
+++ src/app/components/code-mirror-container/code-mirror-container.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CodeMirrorContainerComponent } from './code-mirror-container.component';
+
+describe('CodeMirrorContainerComponent', () => {
+  let component: CodeMirrorContainerComponent;
+  let fixture: ComponentFixture<CodeMirrorContainerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CodeMirrorContainerComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(CodeMirrorContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
