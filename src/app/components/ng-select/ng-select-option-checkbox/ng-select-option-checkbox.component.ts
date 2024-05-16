dd45a6173d01ba83d59c4e71bd18bfebaaab6e48
nook24 <info@nook24.eu>
2024-05-16 11:42:41 +0200

    Add ngOptionHighlight

--- src/app/components/ng-select/ng-select-option-checkbox/ng-select-option-checkbox.component.ts
+++ src/app/components/ng-select/ng-select-option-checkbox/ng-select-option-checkbox.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
 
 @Component({
     selector: 'oitc-ng-select-option-checkbox',
     standalone: true,
     imports: [
-        FormsModule
+        FormsModule,
+        NgOptionHighlightModule
     ],
     templateUrl: './ng-select-option-checkbox.component.html',
     styleUrl: './ng-select-option-checkbox.component.css'
@@ -14,4 +16,5 @@ export class NgSelectOptionCheckboxComponent {
     @Input() item: any;
     @Input() item$: any;
     @Input() index!: number;
+    @Input() search!: string;
 }
