e09a2439d1fee92383f6031cfa9586d8c878bde5
nook24 <info@nook24.eu>
2024-05-27 09:52:39 +0200

    Implement performance info with sparklines

--- src/app/pages/nagiostats/nagiostats-index/nagiostats-index.component.ts
+++ src/app/pages/nagiostats/nagiostats-index/nagiostats-index.component.ts
@@ -5,6 +5,8 @@ import {
     CardHeaderComponent,
     CardTitleDirective,
     ColComponent,
+    NavComponent,
+    NavItemComponent,
     RowComponent,
     TableDirective
 } from '@coreui/angular';
@@ -18,6 +20,8 @@ import { interval, startWith, Subscription, switchMap } from 'rxjs';
 import { NagiostatsService } from '../nagiostats.service';
 import { Nagiostats } from '../nagiostats.interface';
 import { NgClass, NgIf } from '@angular/common';
+import { SparklineStatsComponent } from '../../../components/sparkline-stats/sparkline-stats.component';
+import { XsButtonDirective } from '../../../layouts/coreui/xsbutton-directive/xsbutton.directive';
 
 @Component({
     selector: 'oitc-nagiostats-index',
@@ -37,7 +41,11 @@ import { NgClass, NgIf } from '@angular/common';
         MatSort,
         TableDirective,
         NgClass,
-        NgIf
+        NgIf,
+        SparklineStatsComponent,
+        NavComponent,
+        NavItemComponent,
+        XsButtonDirective
     ],
     templateUrl: './nagiostats-index.component.html',
     styleUrl: './nagiostats-index.component.css'
@@ -45,6 +53,8 @@ import { NgClass, NgIf } from '@angular/common';
 export class NagiostatsIndexComponent implements OnInit, OnDestroy {
 
     public stats: Nagiostats | undefined;
+
+    public lastUpdate: Date = new Date();
 
     private subscriptions: Subscription = new Subscription();
     private readonly NagiostatsService = inject(NagiostatsService)
@@ -59,6 +69,7 @@ export class NagiostatsIndexComponent implements OnInit, OnDestroy {
                 )
                 .subscribe((data: Nagiostats) => {
                     this.stats = data;
+                    this.lastUpdate = new Date();
                 })
         );
     }
@@ -67,5 +78,12 @@ export class NagiostatsIndexComponent implements OnInit, OnDestroy {
         this.subscriptions.unsubscribe();
     }
 
+    public loadStats(): void {
+        this.subscriptions.add(
+            this.NagiostatsService.getIndex().subscribe((data: Nagiostats) => {
+                this.stats = data;
+                this.lastUpdate = new Date();
+            }));
+    }
 
 }
