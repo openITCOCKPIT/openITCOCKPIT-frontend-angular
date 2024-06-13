cd30e24e6236d67970a8eabbab32c677c07d3dd9
ibering <irina.bering@it-novum.com>
2024-06-13 12:27:15 +0200

    ITC-3202 Core: Hostdependencies  (initial commit)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -209,6 +209,9 @@ export const routes: Routes = [{
     path: 'serviceescalations/add',
     loadComponent: () => import('./pages/serviceescalations/serviceescalations-add/serviceescalations-add.component').then(m => m.ServiceescalationsAddComponent)
 }, {
+    path: 'hostdependencies/index',
+    loadComponent: () => import('./pages/hostdependencies/hostdependencies-index/hostdependencies-index.component').then(m => m.HostdependenciesIndexComponent)
+}, {
     path: 'error/404',
     loadComponent: () => import('./layouts/coreui/errors/error404/error404.component').then(m => m.Error404Component)
 }, {
---
+++ src/app/pages/hostdependencies/hostdependencies-index/hostdependencies-index.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'oitc-hostdependencies-index',
+  standalone: true,
+  imports: [],
+  templateUrl: './hostdependencies-index.component.html',
+  styleUrl: './hostdependencies-index.component.css'
+})
+export class HostdependenciesIndexComponent {
+
+}
