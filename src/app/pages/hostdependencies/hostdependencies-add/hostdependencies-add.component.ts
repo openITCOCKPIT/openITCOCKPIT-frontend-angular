ba07442c8ecda4addb8fef18f9536524ed5454fb
ibering <irina.bering@it-novum.com>
2024-06-17 16:14:31 +0200

    ITC-3202 Core: Hostdependencies add view (initial commit)

---
+++ src/app/pages/hostdependencies/hostdependencies-add/hostdependencies-add.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'oitc-hostdependencies-add',
+  standalone: true,
+  imports: [],
+  templateUrl: './hostdependencies-add.component.html',
+  styleUrl: './hostdependencies-add.component.css'
+})
+export class HostdependenciesAddComponent {
+
+}
