4a0dd6668e79e4e2613d402f4b02091a676f5b0d
ibering <irina.bering@it-novum.com>
2024-06-18 12:46:34 +0200

    ITC-3202 Core: Hostdependencies edit view (initial commit)

---
+++ src/app/pages/hostdependencies/hostdependencies-edit/hostdependencies-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'oitc-hostdependencies-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './hostdependencies-edit.component.html',
+  styleUrl: './hostdependencies-edit.component.css'
+})
+export class HostdependenciesEditComponent {
+
+}
