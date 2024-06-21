f2302802e2e90fe70a95e9466e0468d31b029955
ibering <irina.bering@it-novum.com>
2024-06-21 13:01:41 +0200

    ITC-3197 Core: Notifications - hostNotification/serviceNotification (pages for host/service browser)

---
+++ src/app/pages/notifications/host-notification/host-notification.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HostNotificationComponent } from './host-notification.component';
+
+describe('HostNotificationComponent', () => {
+  let component: HostNotificationComponent;
+  let fixture: ComponentFixture<HostNotificationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HostNotificationComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HostNotificationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
