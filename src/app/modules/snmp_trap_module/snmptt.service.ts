6c92b673ce84f1f1ea8a545f400cf1ddc26ee0ed
ibering <irina.bering@it-novum.com>
2024-07-01 07:39:36 +0200

    ITC-3320 SnmpTrapModule: SnmpttList index view (service and interface added)

---
+++ src/app/modules/snmp_trap_module/snmptt.service.ts
@@ -0,0 +1,34 @@
+import { inject, Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { SnmpttEntryIndexParams, SnmpttEntryIndexRoot } from './snmptt.interface';
+import { HttpClient } from '@angular/common/http';
+import { PROXY_PATH } from '../../tokens/proxy-path.token';
+import { DeleteAllItem, DeleteAllModalService } from '../../layouts/coreui/delete-all-modal/delete-all.interface';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SnmpttService implements DeleteAllModalService{
+    private readonly http = inject(HttpClient);
+    private readonly proxyPath = inject(PROXY_PATH);
+
+    constructor() {
+    }
+
+    public getIndex(params: SnmpttEntryIndexParams): Observable<SnmpttEntryIndexRoot> {
+        const proxyPath = this.proxyPath;
+        return this.http.get<SnmpttEntryIndexRoot>(`${proxyPath}/snmp_trap_module/SnmpttList/index.json`, {
+            params: params as {} // cast CommandsIndexParams into object
+        }).pipe(
+            map(data => {
+                return data;
+            })
+        )
+    }
+
+    // Generic function for the Delete All Modal
+    public delete(item: DeleteAllItem): Observable<Object> {
+        const proxyPath = this.proxyPath;
+        return this.http.post(`${proxyPath}/snmp_trap_module/SnmpttList/delete/${item.id}.json?angular=true`, {});
+    }
+}
