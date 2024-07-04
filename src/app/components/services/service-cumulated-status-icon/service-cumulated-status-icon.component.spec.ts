1660beb5eb0b11774d24668732e6c9acfcdf7837
Jan Nox <jan.nox@pm.me>
2024-07-04 09:50:03 +0000

    ITC-3217 * Add new files for previous commit.

---
+++ src/app/components/services/service-cumulated-status-icon/service-cumulated-status-icon.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ServiceCumulatedStatusIconComponent } from './service-cumulated-status-icon.component';
+
+describe('ServiceCumulatedStatusIconComponent', () => {
+  let component: ServiceCumulatedStatusIconComponent;
+  let fixture: ComponentFixture<ServiceCumulatedStatusIconComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ServiceCumulatedStatusIconComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ServiceCumulatedStatusIconComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
