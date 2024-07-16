df7ae5b257a9a423f6da3ec079444f70d9825fae
ibering <irina.bering@it-novum.com>
2024-07-16 15:19:00 +0200

    ITC-3248 Core: Recurring Downtimes - host view

--- src/app/pages/systemdowntimes/systemdowntimes.interface.ts
+++ src/app/pages/systemdowntimes/systemdowntimes.interface.ts
@@ -1,3 +1,38 @@
+import { PaginateOrScroll } from '../../layouts/coreui/paginator/paginator.interface';
+import { HostObject } from '../hosts/hosts.interface';
+
+export interface SystemdowntimeHostIndexRoot extends PaginateOrScroll {
+    all_host_recurring_downtimes: HostSystemdowntime[]
+    _csrfToken: string
+}
+
+export interface HostSystemdowntime {
+    Host: SystemdowntimeHost
+    Systemdowntime: Systemdowntime
+}
+
+
+export interface SystemdowntimeHost {
+    id: string
+    uuid: string
+    hostname: string
+    address: any
+    description: any
+    hosttemplate_id: any
+    active_checks_enabled: any
+    satelliteId: any
+    containerId: any
+    containerIds: any
+    tags: any
+    usageFlag: any
+    allow_edit: boolean
+    disabled: boolean
+    priority: any
+    notes: any
+    is_satellite_host: boolean
+    name: string
+}
+
 export interface SystemdowntimesPost {
     id?: null | number
     is_recurring: number
@@ -29,5 +64,43 @@ export interface SystemdowntimesGet {
         downtimetype_id: string
         object_id: number[]
     }
+}
 
+export interface Systemdowntime {
+    id: number
+    objecttypeId: number
+    objectId: number
+    downtimeTypeId: number
+    weekdays: string[]
+    weekdaysHuman: string[]
+    dayOfMonth: string[]
+    startTime: string
+    duration: number
+    comment: string
+    author: string
+    UserTime: {}
+}
+
+export interface HostSystemdowntimesParams {
+    angular: true,
+    scroll: boolean,
+    sort: string,
+    page: number,
+    direction: 'asc' | 'desc' | '', // asc or desc
+    'filter[Systemdowntimes.author]': '',
+    'filter[Systemdowntimes.comment]': '',
+    'filter[Hosts.name]': ''
+}
+
+export function getDefaultHostSystemdowntimesParams(): HostSystemdowntimesParams {
+    return {
+        angular: true,
+        scroll: true,
+        sort: 'Systemdowntimes.from_time',
+        page: 1,
+        direction: 'desc',
+        'filter[Systemdowntimes.author]': '',
+        'filter[Systemdowntimes.comment]': '',
+        'filter[Hosts.name]': ''
+    }
 }
