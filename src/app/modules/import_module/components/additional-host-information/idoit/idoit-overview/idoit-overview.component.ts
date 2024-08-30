654760aa3e1c09d2cf0b4622a17f455b862296d0
nook24 <info@nook24.eu>
2024-08-30 13:04:48 +0200

    ITC-3294 Add CMDB online-offline badge

--- src/app/modules/import_module/components/additional-host-information/idoit/idoit-overview/idoit-overview.component.ts
+++ src/app/modules/import_module/components/additional-host-information/idoit/idoit-overview/idoit-overview.component.ts
@@ -1,10 +1,24 @@
 import { Component, Input } from '@angular/core';
 import { AdditionalHostInformationResult } from '../../../../ExternalSystems.interface';
+import { TranslocoDirective } from '@jsverse/transloco';
+import { BadgeComponent, ColComponent, RowComponent } from '@coreui/angular';
+import { HostgroupExtendedTabs } from '../../../../../../pages/hostgroups/hostgroups.enum';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { NgIf } from '@angular/common';
+import { OnlineOfflineComponent } from '../../online-offline/online-offline.component';
 
 @Component({
     selector: 'oitc-idoit-overview',
     standalone: true,
-    imports: [],
+    imports: [
+        TranslocoDirective,
+        ColComponent,
+        RowComponent,
+        BadgeComponent,
+        FaIconComponent,
+        NgIf,
+        OnlineOfflineComponent
+    ],
     templateUrl: './idoit-overview.component.html',
     styleUrl: './idoit-overview.component.css'
 })
@@ -12,4 +26,5 @@ export class IdoitOverviewComponent {
 
     @Input() public result!: AdditionalHostInformationResult;
 
+    protected readonly HostgroupExtendedTabs = HostgroupExtendedTabs;
 }
--- src/app/modules/import_module/components/additional-host-information/itop/itop-overview/itop-overview.component.ts
+++ src/app/modules/import_module/components/additional-host-information/itop/itop-overview/itop-overview.component.ts
@@ -1,10 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { AdditionalHostInformationResult } from '../../../../ExternalSystems.interface';
+import { ColComponent, RowComponent } from '@coreui/angular';
+import { OnlineOfflineComponent } from '../../online-offline/online-offline.component';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
     selector: 'oitc-itop-overview',
     standalone: true,
-    imports: [],
+    imports: [
+        ColComponent,
+        OnlineOfflineComponent,
+        RowComponent,
+        TranslocoDirective
+    ],
     templateUrl: './itop-overview.component.html',
     styleUrl: './itop-overview.component.css'
 })
