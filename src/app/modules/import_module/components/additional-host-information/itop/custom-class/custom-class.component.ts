b4c99518d8affabaa5dbf6b7ec7186c69bfed1e4
ibering <irina.bering@it-novum.com>
2024-09-02 16:12:48 +0200

    #ITC-3294 ImportModule: ExternalSystems - cmdb views (custom class added)

--- src/app/modules/import_module/components/additional-host-information/itop/custom-class/custom-class.component.ts
+++ src/app/modules/import_module/components/additional-host-information/itop/custom-class/custom-class.component.ts
@@ -1,12 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { AdditionalHostInformationResult } from '../../../../ExternalSystems.interface';
+import { FaIconComponent, FaLayersComponent } from '@fortawesome/angular-fontawesome';
+import { TableDirective } from '@coreui/angular';
+import { TranslocoDirective } from '@jsverse/transloco';
+import { NgIf } from '@angular/common';
 
 @Component({
-  selector: 'oitc-custom-class',
-  standalone: true,
-  imports: [],
-  templateUrl: './custom-class.component.html',
-  styleUrl: './custom-class.component.css'
+    selector: 'oitc-custom-class',
+    standalone: true,
+    imports: [
+        FaIconComponent,
+        FaLayersComponent,
+        TableDirective,
+        TranslocoDirective,
+        NgIf
+    ],
+    templateUrl: './custom-class.component.html',
+    styleUrl: './custom-class.component.css'
 })
 export class CustomClassComponent {
     @Input() result!: AdditionalHostInformationResult;
