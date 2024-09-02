b509e5013a317e1c06f4602bc9d3bae6e07d2477
ibering <irina.bering@it-novum.com>
2024-09-02 14:03:28 +0200

    #ITC-3294 ImportModule: ExternalSystems - cmdb views (Virtual Machine)

--- src/app/modules/import_module/components/additional-host-information/itop/virtual-machine/virtual-machine.component.ts
+++ src/app/modules/import_module/components/additional-host-information/itop/virtual-machine/virtual-machine.component.ts
@@ -1,10 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { AdditionalHostInformationResult } from '../../../../ExternalSystems.interface';
+import { FaIconComponent, FaLayersComponent } from '@fortawesome/angular-fontawesome';
+import { TableDirective } from '@coreui/angular';
+import { TranslocoDirective, TranslocoPipe } from '@jsverse/transloco';
+import { NgForOf, NgSwitch, NgSwitchCase } from '@angular/common';
 
 @Component({
   selector: 'oitc-virtual-machine',
   standalone: true,
-  imports: [],
+    imports: [
+        FaIconComponent,
+        FaLayersComponent,
+        TableDirective,
+        TranslocoDirective,
+        NgSwitchCase,
+        NgSwitch,
+        TranslocoPipe,
+        NgForOf
+    ],
   templateUrl: './virtual-machine.component.html',
   styleUrl: './virtual-machine.component.css'
 })
