066ebf15ac19656eef81bd3fe1d5ad1b2330a80a
ibering <irina.bering@it-novum.com>
2024-09-04 15:46:03 +0200

    #ITC-3294 ImportModule: ExternalSystems - host group summary view (first steps)

--- src/app/modules/import_module/components/dependency-tree/host-group-summary/host-group-summary.component.ts
+++ src/app/modules/import_module/components/dependency-tree/host-group-summary/host-group-summary.component.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { PermissionDirective } from '../../../../../permissions/permission.directive';
+import { RouterLink } from '@angular/router';
+import { JsonPipe, KeyValuePipe, NgForOf, NgIf } from '@angular/common';
+import { TranslocoDirective, TranslocoService } from '@jsverse/transloco';
+import { BadgeComponent, TableDirective } from '@coreui/angular';
+import { HostgroupSummaryState, SummaryState } from '../../../../../pages/hosts/summary_state.interface';
+import { NodeExtended } from '../dependency-tree.component';
 
 @Component({
   selector: 'oitc-host-group-summary',
   standalone: true,
-  imports: [],
+    imports: [
+        FaIconComponent,
+        PermissionDirective,
+        RouterLink,
+        NgIf,
+        TranslocoDirective,
+        JsonPipe,
+        BadgeComponent,
+        NgForOf,
+        TableDirective,
+        KeyValuePipe
+    ],
   templateUrl: './host-group-summary.component.html',
   styleUrl: './host-group-summary.component.css'
 })
 export class HostGroupSummaryComponent {
+    @Input() hostgroupSummeryState!: HostgroupSummaryState;
+    @Input() selectedNode!: NodeExtended;
+    public TranslocoService: TranslocoService = inject(TranslocoService);
 
+    public hostSummaryRowLabels: { key: keyof SummaryState, label: string, queryParams: {} }[] = [
+        {
+            key: 'state',
+            label: this.TranslocoService.translate('State'),
+            queryParams: {
+            }
+        }, {
+            key: 'acknowledged',
+            label: this.TranslocoService.translate('Acknowledged'),
+            queryParams: {
+                'acknowledged': true,
+
+            }
+        }, {
+            key: 'in_downtime',
+            label: this.TranslocoService.translate('In downtime'),
+            queryParams: {
+                'in_downtime': true
+            }
+        }, {
+            key: 'not_handled',
+            label: this.TranslocoService.translate('Not handled'),
+            queryParams: {
+                'not_acknowledged': true,
+                'not_in_downtime': true
+            }
+        }, {
+            key: 'passive',
+            label: this.TranslocoService.translate('Passive'),
+            queryParams: {
+                'passive': true
+            }
+        }
+    ];
+    protected readonly Object = Object;
 }
--- src/app/modules/import_module/components/dependency-tree/not-in-monitoring/not-in-monitoring.component.ts
+++ src/app/modules/import_module/components/dependency-tree/not-in-monitoring/not-in-monitoring.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { BadgeComponent, TableDirective } from '@coreui/angular';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { NgForOf, NgIf } from '@angular/common';
+import { PermissionDirective } from '../../../../../permissions/permission.directive';
+import { TranslocoDirective } from '@jsverse/transloco';
+import { NodeExtended } from '../dependency-tree.component';
 
 @Component({
-  selector: 'oitc-not-in-monitoring',
-  standalone: true,
-  imports: [],
-  templateUrl: './not-in-monitoring.component.html',
-  styleUrl: './not-in-monitoring.component.css'
+    selector: 'oitc-not-in-monitoring',
+    standalone: true,
+    imports: [
+        BadgeComponent,
+        FaIconComponent,
+        NgForOf,
+        NgIf,
+        PermissionDirective,
+        TableDirective,
+        TranslocoDirective
+    ],
+    templateUrl: './not-in-monitoring.component.html',
+    styleUrl: './not-in-monitoring.component.css'
 })
 export class NotInMonitoringComponent {
+    @Input() selectedNode!: NodeExtended;
 
 }
