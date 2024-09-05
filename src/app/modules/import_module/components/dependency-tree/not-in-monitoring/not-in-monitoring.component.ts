160f0a5a5755d3bd20bb428ec84896ef1e89e4f0
nook24 <info@nook24.eu>
2024-09-05 12:07:37 +0200

    Add Systemname Service

--- src/app/modules/import_module/components/dependency-tree/not-in-monitoring/not-in-monitoring.component.ts
+++ src/app/modules/import_module/components/dependency-tree/not-in-monitoring/not-in-monitoring.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { BadgeComponent, TableDirective } from '@coreui/angular';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { NgForOf, NgIf } from '@angular/common';
+import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
 import { PermissionDirective } from '../../../../../permissions/permission.directive';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { NodeExtended } from '../dependency-tree.component';
+import { SystemnameService } from '../../../../../services/systemname.service';
 
 @Component({
     selector: 'oitc-not-in-monitoring',
@@ -16,12 +17,16 @@ import { NodeExtended } from '../dependency-tree.component';
         NgIf,
         PermissionDirective,
         TableDirective,
-        TranslocoDirective
+        TranslocoDirective,
+        AsyncPipe
     ],
     templateUrl: './not-in-monitoring.component.html',
     styleUrl: './not-in-monitoring.component.css'
 })
 export class NotInMonitoringComponent {
     @Input() selectedNode!: NodeExtended;
+
+    public readonly SystemnameService = inject(SystemnameService);
+
 
 }
