d3ef1bfa782435d9124e2ea5834965f8967f9af7
abergen <alex.bergen@it-novum.com>
2024-09-20 15:14:45 +0000

    ITC-3327 - added host mapping rules service and interface

---
+++ src/app/modules/sla_module/pages/slas/HostMappingRules.interface.ts
@@ -0,0 +1,143 @@
+import { Sla } from './Slas.interface';
+import { PaginateOrScroll } from '../../../../layouts/coreui/paginator/paginator.interface';
+
+export interface HostMappingRulesPost {
+    host_keywords: null | string[]
+    host_not_keywords: null | string[]
+    hostname_regex: string
+    description: string
+    service_keywords: null | string[]
+    service_not_keywords: null | string[]
+    servicename_regex: string
+    save_options: boolean
+    sla_id: number
+    only_unassigned: boolean
+    filter?: Filter
+}
+
+export interface Filter {
+    'Hosts.name': string
+    'Hosts.keywords': any[]
+    'Hosts.not_keywords': any[]
+    'servicename': string
+    'Services.keywords': any[]
+    'Services.not_keywords': any[]
+}
+
+export interface HostMappingRulesAssignToHostsRoot {
+    sla: Sla
+    _csrfToken: string;
+}
+
+export function getDefaultHostMappingRulesPost(id: number): HostMappingRulesPost {
+    return {
+        host_keywords: null,
+        host_not_keywords: null,
+        hostname_regex: '',
+        description: '',
+        service_keywords: null,
+        service_not_keywords: null,
+        servicename_regex: '',
+        save_options: false,
+        sla_id: id,
+        only_unassigned: true
+
+    }
+}
+
+export interface HostMappingRulesLoadHostsParams {
+    angular: true
+    scroll: boolean
+    sort: string
+    page: number
+    direction: 'asc' | 'desc' | '' // asc or desc
+    slaId: number
+    'filter[Hosts.name]': string
+    'filter[hostdescription]': string
+    'filter[Hosts.keywords][]': string[]
+    'filter[Hosts.not_keywords][]': string[]
+    'filter[servicename]': string
+    'filter[Services.keywords][]': string[]
+    'filter[Services.not_keywords][]': string[]
+    resolveContainerIds: true
+    onlyUnassigned: boolean
+}
+
+export function getDefaultHostMappingRulesLoadHostsParams(id: number): HostMappingRulesLoadHostsParams {
+    return {
+        angular: true,
+        scroll: true,
+        sort: 'Hosts.name',
+        page: 1,
+        direction: 'asc',
+        slaId: id,
+        'filter[Hosts.name]': '',
+        'filter[hostdescription]': '',
+        'filter[Hosts.keywords][]': [],
+        'filter[Hosts.not_keywords][]': [],
+        'filter[servicename]': '',
+        'filter[Services.keywords][]': [],
+        'filter[Services.not_keywords][]': [],
+        resolveContainerIds: true,
+        onlyUnassigned: true
+
+    }
+}
+
+export interface LoadHostsRoot extends PaginateOrScroll {
+    hosts: Host[]
+    _csrfToken: string;
+}
+
+export interface Host {
+    id: number
+    name: string
+    address: string
+    sla_id: number
+    hostdescription: string
+    hosttags: string
+    container: string
+    primary_container: string
+    services: Service[]
+    hosttemplate: Hosttemplate
+    hosts_to_containers_sharing: HostsToContainersSharing[]
+    sla: Sla
+}
+
+export interface Service {
+    id: number
+    host_id: number
+    servicename: string
+    description: any
+    servicetags: string
+    sla_relevant: any
+    is_sla_relevant: number
+    servicetemplate: Servicetemplate
+}
+
+export interface Servicetemplate {
+    id: number
+    sla_relevant: number
+}
+
+export interface Hosttemplate {
+    id: number
+    sla_id: any
+}
+
+export interface HostsToContainersSharing {
+    id: number
+    containertype_id: number
+    name: string
+    parent_id: any
+    lft: number
+    rght: number
+    _joinData: JoinData
+}
+
+export interface JoinData {
+    id: number
+    host_id: number
+    container_id: number
+}
+
---
+++ src/app/modules/sla_module/pages/slas/HostMappingRules.service.ts
@@ -0,0 +1,61 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PROXY_PATH } from '../../../../tokens/proxy-path.token';
+import { catchError, map, Observable, of } from 'rxjs';
+import { GenericIdResponse, GenericResponseWrapper, GenericValidationError } from '../../../../generic-responses';
+import {
+    HostMappingRulesAssignToHostsRoot,
+    HostMappingRulesLoadHostsParams,
+    HostMappingRulesPost,
+    LoadHostsRoot
+} from './HostMappingRules.interface';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class HostMappingRulesService {
+    private readonly http: HttpClient = inject(HttpClient);
+    private readonly proxyPath: string = inject(PROXY_PATH);
+
+    public loadAssignToHosts(id: number): Observable<HostMappingRulesAssignToHostsRoot> {
+        const proxyPath = this.proxyPath;
+        return this.http.get<HostMappingRulesAssignToHostsRoot>(`${proxyPath}/sla_module/host_mapping_rules/assignToHosts/${id}.json?angular=true`).pipe(
+            map(data => {
+                return data;
+            })
+        )
+    }
+
+    public loadHosts(params: HostMappingRulesLoadHostsParams): Observable<LoadHostsRoot> {
+        const proxyPath: string = this.proxyPath;
+        return this.http.get<LoadHostsRoot>(`${proxyPath}/sla_module/host_mapping_rules/loadHostsBySlaId.json?angular=true`, {
+            params: params as {}
+        }).pipe(
+            map((data: LoadHostsRoot) => {
+                return data;
+            })
+        )
+    }
+
+    public assignToHosts(post: HostMappingRulesPost, id: number): Observable<GenericResponseWrapper> {
+        const proxyPath = this.proxyPath;
+        return this.http.post<any>(`${proxyPath}/sla_module/host_mapping_rules/assignToHosts/${id}.json?angular=true`, post)
+            .pipe(
+                map(data => {
+                    // Return true on 200 Ok
+                    return {
+                        success: true,
+                        data: data as GenericIdResponse
+                    };
+                }),
+                catchError((error: any) => {
+                    const err = error.error.error as GenericValidationError;
+                    return of({
+                        success: false,
+                        data: err
+                    });
+                })
+            );
+    }
+
+}
