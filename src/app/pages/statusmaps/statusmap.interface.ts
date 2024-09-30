ca316fe2cebfc1a8cf9058f52f00b4b9fc9b948b
nook24 <info@nook24.eu>
2024-09-30 13:49:09 +0200

    ITC-3216 Implement basic status map feature

---
+++ src/app/pages/statusmaps/statusmap.interface.ts
@@ -0,0 +1,33 @@
+import { Edge, Node } from 'vis-network';
+import { SelectKeyValue } from '../../layouts/primeng/select.interface';
+
+/**********************
+ *    Index action    *
+ **********************/
+export interface StatusmapsIndexParmas {
+    angular: true,
+    'filter[Hosts.name]': string
+    'filter[Hosts.address]': string
+    'filter[Hosts.satellite_id]': number
+    'showAll': boolean
+}
+
+export function getDefaultStatusmapsIndexParams(): StatusmapsIndexParmas {
+    return {
+        angular: true,
+        'filter[Hosts.name]': "",
+        'filter[Hosts.address]': "",
+        'filter[Hosts.satellite_id]': 0,
+        'showAll': false
+    }
+}
+
+export interface StatusmapsIndexRootResult {
+    statusMap: {
+        nodes: Node[]
+        edges: Edge[]
+    },
+    hasBrowserRights: boolean,
+    satellites: SelectKeyValue[],
+    _csrfToken: string
+}
