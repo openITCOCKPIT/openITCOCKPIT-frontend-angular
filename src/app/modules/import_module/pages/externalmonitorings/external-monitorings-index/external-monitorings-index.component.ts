8132410590922f34541d769815cd5f5f52f3ef42
ibering <irina.bering@it-novum.com>
2024-10-01 10:37:13 +0200

    ITC-3298 ImportModule: ExternalMonitorings index view (initial import)

---
+++ src/app/modules/import_module/pages/externalmonitorings/external-monitorings-index/external-monitorings-index.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'oitc-external-monitorings-index',
+  standalone: true,
+  imports: [],
+  templateUrl: './external-monitorings-index.component.html',
+  styleUrl: './external-monitorings-index.component.css'
+})
+export class ExternalMonitoringsIndexComponent {
+
+}
