911db75b657350bc5cdf03b15a547b47c900bf86
nook24 <info@nook24.eu>
2024-10-11 10:40:56 +0200

    Remove function class from templates

---
+++ src/app/components/charts/host-pie-echart/host-pie-echart.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HostPieEchartComponent } from './host-pie-echart.component';
+
+describe('HostPieEchartComponent', () => {
+  let component: HostPieEchartComponent;
+  let fixture: ComponentFixture<HostPieEchartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HostPieEchartComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HostPieEchartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
