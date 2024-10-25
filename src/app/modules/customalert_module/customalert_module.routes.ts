9e5515c3eab85182261539240ef48c188c41eded
Jan Nox <jan.nox@pm.me>
2024-10-25 07:37:35 +0000

    ITC-3325 * Create the add page.

--- src/app/modules/customalert_module/customalert_module.routes.ts
+++ src/app/modules/customalert_module/customalert_module.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const customalertModuleRoutes: Routes = [
     {
+        path: 'customalert_module/customalert_rules/add',
+        loadComponent: () => import('./pages/customalert-rules/customalert-rules-add/customalert-rules-add.component').then(m => m.CustomalertRulesAddComponent)
+    },
+    {
         path: 'customalert_module/customalert_rules/index',
         loadComponent: () => import('./pages/customalert-rules/customalert-rules-index/customalert-rules-index.component').then(m => m.CustomalertRulesIndexComponent)
     }
--- src/app/modules/customalert_module/pages/customalert-rules/customalert-rules.service.ts
+++ src/app/modules/customalert_module/pages/customalert-rules/customalert-rules.service.ts
@@ -1,9 +1,11 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PROXY_PATH } from '../../../../tokens/proxy-path.token';
-import { map, Observable } from 'rxjs';
-import { CustomAlertRulesIndex, CustomAlertRulesIndexParams } from './customalert-rules.interface';
+import { catchError, map, Observable, of } from 'rxjs';
+import { CustomAlertRule, CustomAlertRulesIndex, CustomAlertRulesIndexParams } from './customalert-rules.interface';
 import { DeleteAllItem } from '../../../../layouts/coreui/delete-all-modal/delete-all.interface';
+import { ServicePost } from '../../../../pages/services/services.interface';
+import { GenericIdResponse, GenericResponseWrapper, GenericValidationError } from '../../../../generic-responses';
 
 @Injectable({
     providedIn: 'root'
@@ -26,5 +28,26 @@ export class CustomalertRulesService {
 
     public delete(item: DeleteAllItem): Observable<Object> {
         return this.http.post(`${this.proxyPath}/customalert_module/customalert_rules/delete/${item.id}.json`, {});
+    }
+
+    public add(customAlertRule: CustomAlertRule): Observable<GenericResponseWrapper> {
+        const proxyPath = this.proxyPath;
+        return this.http.post<any>(`${proxyPath}/customalert_module/customalert_rules/add.json?angular=true`, customAlertRule)
+            .pipe(
+                map(data => {
+                    // Return true on 200 Ok
+                    return {
+                        success: true,
+                        data: data as GenericIdResponse
+                    };
+                }),
+                catchError((error: any) => {
+                    const err = error.error.error as GenericValidationError;
+                    return of({
+                        success: false,
+                        data: err
+                    });
+                })
+            );
     }
 }
