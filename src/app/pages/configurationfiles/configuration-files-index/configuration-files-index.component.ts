b4807519b15385ad9836d83e5a176c00ac4d8a03
nook24 <info@nook24.eu>
2024-10-29 08:22:58 +0100

    ITC-3250 Start migration of Config File Editor

---
+++ src/app/pages/configurationfiles/configuration-files-index/configuration-files-index.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'oitc-configuration-files-index',
+  standalone: true,
+  imports: [],
+  templateUrl: './configuration-files-index.component.html',
+  styleUrl: './configuration-files-index.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ConfigurationFilesIndexComponent {
+
+}
