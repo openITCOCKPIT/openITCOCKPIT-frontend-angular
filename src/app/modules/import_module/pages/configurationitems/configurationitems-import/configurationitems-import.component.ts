556854b18da34cf8d9f665cf9510c4b9d004eda9
nook24 <info@nook24.eu>
2024-11-01 09:06:51 +0100

    Change width of mass actions on all pages

--- src/app/modules/import_module/pages/configurationitems/configurationitems-import/configurationitems-import.component.ts
+++ src/app/modules/import_module/pages/configurationitems/configurationitems-import/configurationitems-import.component.ts
@@ -1,13 +1,76 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import {
+    CardBodyComponent,
+    CardComponent,
+    CardFooterComponent,
+    CardHeaderComponent,
+    CardTitleDirective,
+    ColComponent,
+    FormDirective,
+    FormLabelDirective,
+    NavComponent,
+    NavItemComponent,
+    RowComponent
+} from '@coreui/angular';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { FormLoaderComponent } from '../../../../../layouts/primeng/loading/form-loader/form-loader.component';
+import { FormsModule } from '@angular/forms';
+import { MultiSelectComponent } from '../../../../../layouts/primeng/multi-select/multi-select/multi-select.component';
+import { NgClass, NgIf } from '@angular/common';
+import { PermissionDirective } from '../../../../../permissions/permission.directive';
+import { TranslocoDirective } from '@jsverse/transloco';
+import { UiBlockerComponent } from '../../../../../components/ui-blocker/ui-blocker.component';
+import { XsButtonDirective } from '../../../../../layouts/coreui/xsbutton-directive/xsbutton.directive';
+import { RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { GenericValidationError } from '../../../../../generic-responses';
 
 @Component({
-  selector: 'oitc-configurationitems-import',
-  standalone: true,
-  imports: [],
-  templateUrl: './configurationitems-import.component.html',
-  styleUrl: './configurationitems-import.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'oitc-configurationitems-import',
+    standalone: true,
+    imports: [
+        CardBodyComponent,
+        CardComponent,
+        CardFooterComponent,
+        CardHeaderComponent,
+        CardTitleDirective,
+        FaIconComponent,
+        FormDirective,
+        FormLabelDirective,
+        FormLoaderComponent,
+        FormsModule,
+        MultiSelectComponent,
+        NavComponent,
+        NavItemComponent,
+        NgIf,
+        PermissionDirective,
+        TranslocoDirective,
+        UiBlockerComponent,
+        XsButtonDirective,
+        NgClass,
+        RouterLink,
+        RowComponent,
+        ColComponent
+    ],
+    templateUrl: './configurationitems-import.component.html',
+    styleUrl: './configurationitems-import.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ConfigurationitemsImportComponent {
+export class ConfigurationitemsImportComponent implements OnInit, OnDestroy {
+
+    public errors: GenericValidationError | null = null;
+
+
+    private subscriptions: Subscription = new Subscription();
+
+    public ngOnInit(): void {
+    }
+
+    public ngOnDestroy(): void {
+        this.subscriptions.unsubscribe();
+    }
+
+    public submit() {
+    }
 
 }
