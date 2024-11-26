0a3691817c4a4d468bc40b3ce8386bcb11c92e23
Jan Nox <jan.nox@pm.me>
2024-11-26 10:30:55 +0000

    ITC-3218 * Front-End update

--- src/app/pages/wizards/wizards-index/wizards-index.component.ts
+++ src/app/pages/wizards/wizards-index/wizards-index.component.ts
@@ -1,18 +1,28 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import {
+    AlertComponent,
+    ButtonGroupComponent,
     CardBodyComponent,
     CardComponent,
     CardFooterComponent,
+    CardHeaderComponent,
     ColComponent,
     ModalService,
+    NavComponent,
+    NavItemComponent,
     RowComponent
 } from '@coreui/angular';
 import { WizardsService } from '../wizards.service';
 import { WizardsIndex } from '../wizards.interface';
-import { KeyValuePipe, NgForOf, NgIf, NgOptimizedImage } from '@angular/common';
+import { KeyValuePipe, NgClass, NgForOf, NgIf, NgOptimizedImage } from '@angular/common';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { BadgeOutlineComponent } from '../../../layouts/coreui/badge-outline/badge-outline.component';
+import { PermissionDirective } from '../../../permissions/permission.directive';
+import { TranslocoDirective, TranslocoPipe } from '@jsverse/transloco';
+import { RouterLink } from '@angular/router';
+import { BackButtonDirective } from '../../../directives/back-button.directive';
+import { XsButtonDirective } from '../../../layouts/coreui/xsbutton-directive/xsbutton.directive';
 
 @Component({
     selector: 'oitc-wizards-index',
@@ -28,7 +38,19 @@ import { BadgeOutlineComponent } from '../../../layouts/coreui/badge-outline/bad
         NgOptimizedImage,
         CardFooterComponent,
         FaIconComponent,
-        BadgeOutlineComponent
+        BadgeOutlineComponent,
+        PermissionDirective,
+        TranslocoDirective,
+        RouterLink,
+        CardHeaderComponent,
+        BackButtonDirective,
+        NavComponent,
+        NavItemComponent,
+        XsButtonDirective,
+        ButtonGroupComponent,
+        TranslocoPipe,
+        NgClass,
+        AlertComponent
     ],
     templateUrl: './wizards-index.component.html',
     styleUrl: './wizards-index.component.css',
@@ -40,8 +62,25 @@ export class WizardsIndexComponent implements OnInit, OnDestroy {
     private readonly modalService: ModalService = inject(ModalService);
     private readonly cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
 
+    protected filter: any = {
+        Category: {
+            linux: true,
+            windows: true,
+            database: true,
+            mail: true,
+            network: true,
+            docker: true,
+            macos: true,
+            virtualization: true,
+            hardware: true
+        }
+    }
+
     protected result?: WizardsIndex;
 
+    protected filterCategory(category: string): void {
+
+    }
 
     public ngOnInit() {
         this.loadWizards();
@@ -59,7 +98,14 @@ export class WizardsIndexComponent implements OnInit, OnDestroy {
             }))
     }
 
-    protected filterByCategory(category: string[]): boolean {
+    protected filterByCategory(categories: string[]): boolean {
+        // Traverse all categories
+
+        for (let i = 0; i < categories.length; i++) {
+            if (this.filter.Category[categories[i]]) {
+                return true;
+            }
+        }
         return false;
     }
 
