fb6844cc2633f8426054417959db98c5dbb3b376
Jan Nox <jan.nox@pm.me>
2024-12-02 08:37:27 +0000

    ITC-3218 * Add the host configuration step.

---
+++ src/app/pages/wizards/wizards-wizard-host-configuration/wizards-wizard-host-configuration.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WizardsWizardHostConfigurationComponent } from './wizards-wizard-host-configuration.component';
+
+describe('WizardsWizardHostConfigurationComponent', () => {
+  let component: WizardsWizardHostConfigurationComponent;
+  let fixture: ComponentFixture<WizardsWizardHostConfigurationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [WizardsWizardHostConfigurationComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WizardsWizardHostConfigurationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
