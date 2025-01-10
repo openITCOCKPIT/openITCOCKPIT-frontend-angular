37754142ed7e97d06549f411652c3eac7c00b4f1
nook24 <info@nook24.eu>
2025-01-10 15:25:13 +0100

    ITC-3243 Implement Create Backups

--- src/app/pages/backups/backups.interface.ts
+++ src/app/pages/backups/backups.interface.ts
@@ -1,2 +1,18 @@
-export interface Backups {
+export interface StartBackupResponse {
+    backup: {
+        backupRunning: boolean
+        error: boolean
+    }
+    _csrfToken: string
+}
+
+export interface CheckBackupFinishedResponse {
+    backupFinished: {
+        backupFinished: boolean,
+        error: boolean,
+        backup_files: {
+            [key: string]: string
+        }
+    }
+    _csrfToken: string
 }
--- src/app/pages/backups/backups.service.ts
+++ src/app/pages/backups/backups.service.ts
@@ -1,9 +1,55 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PROXY_PATH } from '../../tokens/proxy-path.token';
+import { catchError, map, Observable, of } from 'rxjs';
+import { GenericResponseWrapper, GenericValidationError } from '../../generic-responses';
+import { CheckBackupFinishedResponse, StartBackupResponse } from './backups.interface';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class BackupsService {
 
-  constructor() { }
+    private readonly http = inject(HttpClient);
+    private readonly proxyPath = inject(PROXY_PATH);
+
+    constructor() {
+    }
+
+    /**********************
+     *    Index action    *
+     **********************/
+    public createBackup(filename: string): Observable<GenericResponseWrapper> {
+        const proxyPath = this.proxyPath;
+
+        return this.http.post<any>(`${proxyPath}/backups/backup.json`, {
+            filename: filename
+        })
+            .pipe(
+                map(data => {
+                    // Return true on 200 Ok
+                    return {
+                        success: true,
+                        data: data as StartBackupResponse
+                    };
+                }),
+                catchError((error: any) => {
+                    const err = error.error.error as GenericValidationError;
+                    return of({
+                        success: false,
+                        data: err
+                    });
+                })
+            );
+    }
+
+    public checkBackupFinished(): Observable<CheckBackupFinishedResponse> {
+        const proxyPath = this.proxyPath;
+        return this.http.get<CheckBackupFinishedResponse>(`${proxyPath}/backups/checkBackupFinished.json`).pipe(
+            map(data => {
+                return data;
+            })
+        );
+    }
+
 }
