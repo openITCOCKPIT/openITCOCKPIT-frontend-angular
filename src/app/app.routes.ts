fb2d0ba6873a04b487a09dc35212f379c0fb7834
nook24 <info@nook24.eu>
2025-01-10 11:47:20 +0100

    ITC-3243 Add basic files for Backup and Restore

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -661,6 +661,9 @@ const coreRoutes: Routes = [{
     path: 'ConfigurationFiles/edit/:dbKey',
     loadComponent: () => import('./pages/configurationfiles/configuration-files-edit/configuration-files-edit.component').then(m => m.ConfigurationFilesEditComponent)
 }, {
+    path: 'backups/index',
+    loadComponent: () => import('./pages/backups/backups-index/backups-index.component').then(m => m.BackupsIndexComponent)
+}, {
     path: 'error/403',
     loadComponent: () => import('./layouts/coreui/errors/error403/error403.component').then(m => m.Error403Component)
 }, {
---
+++ src/app/pages/backups/backups-index/backups-index.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'oitc-backups-index',
+  imports: [],
+  templateUrl: './backups-index.component.html',
+  styleUrl: './backups-index.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BackupsIndexComponent {
+
+}
