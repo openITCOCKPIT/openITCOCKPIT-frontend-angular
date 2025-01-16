a142d06accd253b2e315d09d978aa9818a5b8f51
abergen <alex.bergen@it-novum.com>
2025-01-16 11:08:26 +0000

    ITC-3269 - added background image and overflow for small screens

--- src/app/modules/map_module/components/map-canvas/map-canvas.component.ts
+++ src/app/modules/map_module/components/map-canvas/map-canvas.component.ts
@@ -5,16 +5,18 @@ import {
     ElementRef,
     inject,
     input,
+    InputSignal,
     ViewChild
 } from '@angular/core';
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { Helplines } from './map-canvas.interface';
 
 @Component({
     selector: 'oitc-map-canvas',
     standalone: true,
     imports: [
-        NgClass
+        NgClass,
+        NgIf
     ],
     templateUrl: './map-canvas.component.html',
     styleUrl: './map-canvas.component.css',
@@ -26,6 +28,8 @@ export class MapCanvasComponent {
     private cdr = inject(ChangeDetectorRef);
 
     public helplines = input<Helplines>({enabled: true, size: 15});
+    public background = input<string>('');
+    public isViewMode: InputSignal<boolean> = input<boolean>(true);
 
     constructor() {
     }
