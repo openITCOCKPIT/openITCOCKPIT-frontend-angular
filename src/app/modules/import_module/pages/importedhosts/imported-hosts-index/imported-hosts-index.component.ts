941edcddd6c42f648c975a86d062e570966a4913
ibering <irina.bering@it-novum.com>
2025-01-22 12:21:15 +0100

    #ITC-3296 ImportModule: ImportedHosts (initial commit)

---
+++ src/app/modules/import_module/pages/importedhosts/imported-hosts-index/imported-hosts-index.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'oitc-imported-hosts-index',
+  imports: [],
+  templateUrl: './imported-hosts-index.component.html',
+  styleUrl: './imported-hosts-index.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ImportedHostsIndexComponent {
+
+}
