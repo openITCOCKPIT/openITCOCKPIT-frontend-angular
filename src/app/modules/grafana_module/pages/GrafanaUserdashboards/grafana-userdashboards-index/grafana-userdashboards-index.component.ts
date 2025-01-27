0877cb551933e3e44efe30bdba35d23916401a1e
nook24 <info@nook24.eu>
2025-01-27 15:23:14 +0100

    ITC-3268 Generate Grafana Userdashboard Files

---
+++ src/app/modules/grafana_module/pages/GrafanaUserdashboards/grafana-userdashboards-index/grafana-userdashboards-index.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'oitc-grafana-userdashboards-index',
+  imports: [],
+  templateUrl: './grafana-userdashboards-index.component.html',
+  styleUrl: './grafana-userdashboards-index.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class GrafanaUserdashboardsIndexComponent {
+
+}
---
+++ src/app/modules/grafana_module/pages/GrafanaUserdashboards/grafana-userdashboards-view/grafana-userdashboards-view.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GrafanaUserdashboardsViewComponent } from './grafana-userdashboards-view.component';
+
+describe('GrafanaUserdashboardsViewComponent', () => {
+  let component: GrafanaUserdashboardsViewComponent;
+  let fixture: ComponentFixture<GrafanaUserdashboardsViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [GrafanaUserdashboardsViewComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GrafanaUserdashboardsViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
