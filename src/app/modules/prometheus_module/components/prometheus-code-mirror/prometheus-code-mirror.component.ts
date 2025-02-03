a8606be1ecd8dcee464904bedfb66f884ce3d668
Jan Nox <jan.nox@pm.me>
2025-02-03 15:25:33 +0000

    ITC_3310 * Remove unused features from codemirror.

--- src/app/modules/prometheus_module/components/prometheus-code-mirror/prometheus-code-mirror.component.ts
+++ src/app/modules/prometheus_module/components/prometheus-code-mirror/prometheus-code-mirror.component.ts
@@ -11,17 +11,15 @@ import {
     OnDestroy,
     ViewChild
 } from '@angular/core';
-import { Extension } from '@codemirror/state';
+import { EditorState, Extension, StateEffect } from '@codemirror/state';
 import { PromQLExtension } from '@prometheus-io/codemirror-promql';
 import { basicSetup, EditorView } from 'codemirror';
-import { EditorState, Range, RangeSet, StateEffect, StateField } from '@codemirror/state';
-import { Decoration, DecorationSet, keymap } from '@codemirror/view';
+import { keymap } from '@codemirror/view';
 import { defaultKeymap } from '@codemirror/commands';
 import { autocompletion, completeFromList, Completion, } from '@codemirror/autocomplete';
 import {
     AutocompleteItem,
     DefaultMacros,
-    HighlightPattern,
     Macros
 } from '../../../../../app/components/code-mirror-container/code-mirror-container.interface';
 import { MacroIndex } from "../../../../pages/macros/macros.interface";
@@ -51,7 +49,6 @@ export class PrometheusCodeMirrorComponent implements AfterViewInit, OnDestroy {
     @ViewChild('container', {static: true}) containerRef!: ElementRef<HTMLDivElement>;
     @ContentChild(NgModel) textareaModel!: NgModel;
     private editor!: EditorView;
-    private customHighlightExtension!: StateField<DecorationSet>[];
     private autocompleteExtension: any;
     private subscriptions: Subscription = new Subscription();
     private readonly TranslocoService = inject(TranslocoService);
@@ -65,28 +62,6 @@ export class PrometheusCodeMirrorComponent implements AfterViewInit, OnDestroy {
         }));
     }
 
-    private defaultHighlightPatterns: HighlightPattern[] = [
-        {
-            highlight: /(\$ARG\d+\$)/g,
-            className: 'highlight-command-argument'
-        },
-        {
-            highlight: /(\$USER\d+\$)/g,
-            className: 'highlight-user-macro'
-        },
-        {
-            highlight: /(\$_HOST.*?\$)/g,
-            className: 'highlight-custom-macro'
-        },
-        {
-            highlight: /(\$_SERVICE.*?\$)/g,
-            className: 'highlight-custom-macro'
-        },
-        {
-            highlight: /(\$_CONTACT.*?\$)/g,
-            className: 'highlight-custom-macro'
-        }
-    ];
 
     // border focus style
     // only this way is possible, because codemirror overwrites the set css classes
@@ -107,25 +82,15 @@ export class PrometheusCodeMirrorComponent implements AfterViewInit, OnDestroy {
         keymap.of([...defaultKeymap])
     ];
 
-    private _highlightPatterns: HighlightPattern[] =
-        this.defaultHighlightPatterns;
-    private _autocompleteWords: AutocompleteItem[] = this.getNagiosMacroNames();
-    //Macros are overwriting highlightPatterns and autocompleteWords from user
     private defaultMacros: DefaultMacros[] = [];
     private _macros: MacroIndex[] = [];
     @Input({transform: numberAttribute}) rows: number = 6;
 
-    @Input() set highlightPatterns(highlightPatterns: HighlightPattern[]) {
-        this._highlightPatterns = highlightPatterns;
-        this.updateCodemirrorExtension(CodeMirrorUpdateType.HIGHLIGHT);
-    }
 
     @Input() set autocompleteWords(autocompleteWords: AutocompleteItem[]) {
-        this._autocompleteWords = autocompleteWords;
         this.updateCodemirrorExtension(CodeMirrorUpdateType.AUTOCOMPLETE);
     };
 
-    //Macros are overwriting highlightPatterns and autocompleteWords from user
     @Input() set macros(macros: DefaultMacros[] | MacroIndex[] | Macros) {
 
         if (Array.isArray(macros)) {
@@ -142,8 +107,6 @@ export class PrometheusCodeMirrorComponent implements AfterViewInit, OnDestroy {
             this._macros = macros.macros;
         }
 
-        this.addMacrosToHighlight();
-        this.addMacrosToAutocompletion();
         this.updateCodemirrorExtension(CodeMirrorUpdateType.MACROS);
         this.cdr.markForCheck();
     };
@@ -164,12 +127,6 @@ export class PrometheusCodeMirrorComponent implements AfterViewInit, OnDestroy {
                 );
             }
 
-
-            this.autocompleteExtension = this.createAutoCompleteExtension(this._autocompleteWords);
-
-            this.customHighlightExtension = this.createHighlightExtension(
-                this._highlightPatterns
-            );
 
             if (this.theme === 'dark') {
                 this.baseExtensions.push(oneDark);
@@ -210,47 +167,43 @@ export class PrometheusCodeMirrorComponent implements AfterViewInit, OnDestroy {
         this.cdr.markForCheck();
 
         switch (updateType) {
-            case CodeMirrorUpdateType.HIGHLIGHT:
-                this.customHighlightExtension = this.createHighlightExtension(
-                    this._highlightPatterns
-                );
-                break
             case CodeMirrorUpdateType.AUTOCOMPLETE:
-                this.autocompleteExtension = this.createAutoCompleteExtension(this._autocompleteWords);
                 break;
             case CodeMirrorUpdateType.MACROS:
-                this.autocompleteExtension = this.createAutoCompleteExtension(this._autocompleteWords);
-                this.customHighlightExtension = this.createHighlightExtension(
-                    this._highlightPatterns
-                );
                 break;
             default:
                 break;
         }
 
         let updateValueChangeCodemirrorListener = this.setUpdateValueChangeCodemirrorListener();
-
+        console.log('HIER BIN ICH');
         if (this.editor !== undefined && this.editor !== null) {
+            console.log('HIER BIN ICH');
             this.editor.dispatch({
                 changes: {from: 0, to: this.editor.state.doc.length, insert: this.textareaModel.value},
-                effects: StateEffect.reconfigure.of([this.baseExtensions, this.autocompleteExtension, this.customHighlightExtension, updateValueChangeCodemirrorListener]),
+                effects: StateEffect.reconfigure.of([this.baseExtensions, this.autocompleteExtension, updateValueChangeCodemirrorListener]),
             });
+            console.log('HIER BIN ICH');
         }
     }
 
     // sets the listener that updates changes in the codemirror with the textarea
     private setUpdateValueChangeCodemirrorListener() {
+        console.log('HIER BIN ICH');
         const textarea = this.containerRef.nativeElement.querySelector('textarea');
 
         let updateValueChangeCodemirrorListener;
 
-        if (textarea) {
+        if (textarea) {console.log('HIER BIN ICH');
             // listener that updates changes in the codemirror with the textarea
             updateValueChangeCodemirrorListener = EditorView.updateListener.of((update) => {
+                console.log('HIER BIN ICH');
                 if (update.docChanged) {
+                    console.log('HIER BIN ICH');
                     textarea.value = this.editor.state.doc.toString();
                     textarea.dispatchEvent(new Event('input'));
                 }
+                console.log('HIER BIN ICH');
             });
 
         }
@@ -270,108 +223,6 @@ export class PrometheusCodeMirrorComponent implements AfterViewInit, OnDestroy {
         });
     }
 
-    // creates highlighting based on the given list
-    private createHighlightExtension(highlightPatterns: HighlightPattern[]) {
-        const decorationsFromRegex = (doc: string) => {
-            const decorations: Range<Decoration>[] = [];
-
-            highlightPatterns.forEach((pattern) => {
-                const regex = new RegExp(pattern.highlight, 'g');
-                let match;
-                while ((match = regex.exec(doc))) {
-                    const start = match.index;
-                    const end = start + match[0].length;
-                    const deco = Decoration.mark({
-                        class: pattern.className,
-                    });
-                    decorations.push(deco.range(start, end));
-                }
-            });
-
-            // Sort decorations by from position
-            decorations.sort((a, b) => a.from - b.from);
-
-            const rangeSet = RangeSet.of(decorations);
-
-            return <DecorationSet>rangeSet;
-        };
-
-        const field = StateField.define<DecorationSet>({
-            create: (state) => decorationsFromRegex(state.doc.toString()),
-            update: (value, tr) => {
-                if (tr.docChanged) {
-                    return decorationsFromRegex(tr.newDoc.toString());
-                }
-                return value;
-            },
-            provide: (field) => EditorView.decorations.from(field),
-        });
-
-        return [field];
-    }
-
-    private getNagiosMacroNames(): AutocompleteItem[] {
-
-        let autocompleteItems: AutocompleteItem[] = [];
-
-        for (let i = 1; i <= 32; i++) {
-            let item: AutocompleteItem = {
-                value: '$ARG' + i + '$',
-                description: this.TranslocoService.translate('Command Argument')
-            };
-            autocompleteItems.push(item);
-        }
-
-        return autocompleteItems;
-
-    }
-
-    private addMacrosToHighlight() {
-
-        let escapeDollar = new RegExp('\\$', 'g');
-
-        this._highlightPatterns = this.defaultHighlightPatterns;
-
-        for (let index in this.defaultMacros) {
-            for (let i in this.defaultMacros[index].macros) {
-                let macroName = this.defaultMacros[index].macros[i].macro;
-                macroName = macroName.replace(escapeDollar, '\\$');
-                this._highlightPatterns.push({
-                    highlight: new RegExp(macroName, "g"),
-                    className: this.defaultMacros[index].class
-                });
-            }
-        }
-
-    }
-
-    private addMacrosToAutocompletion() {
-
-        let autocompleteItems: AutocompleteItem[] = this.getNagiosMacroNames();
-
-        for (let index in this.defaultMacros) {
-            for (let i in this.defaultMacros[index].macros) {
-                let item: AutocompleteItem = {
-                    value: this.defaultMacros[index].macros[i].macro,
-                    description: this.defaultMacros[index].macros[i].description
-                };
-                autocompleteItems.push(item);
-            }
-        }
-
-        for (let index in this._macros) {
-            let item: AutocompleteItem = {
-                value: this._macros[index].name,
-                description: this._macros[index].description
-            };
-            autocompleteItems.push(item);
-        }
-
-        autocompleteItems.sort((a, b) => a.value.localeCompare(b.value));
-
-        this._autocompleteWords = autocompleteItems;
-
-    }
 
     private isDefaultMacros(obj: any): obj is DefaultMacros {
         return typeof obj === 'object' && 'category' in obj && 'class' in obj && 'macros' in obj;
