f74a32fd984d203ca638f4514a69949508fea2ec
Jan Nox <jan.nox@pm.me>
2025-02-04 15:38:24 +0000

    ITC-3334 * Add edit page (Without calendar, tho).

--- src/app/modules/changecalendar_module/pages/changecalendars/changecalendars.service.ts
+++ src/app/modules/changecalendar_module/pages/changecalendars/changecalendars.service.ts
@@ -2,7 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { PROXY_PATH } from '../../../../tokens/proxy-path.token';
 import { catchError, map, Observable, of } from 'rxjs';
-import { AddChangeCalendar, ChangeCalendarsIndex, ChangeCalendarsIndexParams } from './changecalendars.interface';
+import {
+    AddChangeCalendar,
+    ChangeCalendarsIndex,
+    ChangeCalendarsIndexParams,
+    EditableChangecalendar, EditChangecalendarRoot
+} from './changecalendars.interface';
 import { DeleteAllItem } from '../../../../layouts/coreui/delete-all-modal/delete-all.interface';
 import { GenericIdResponse, GenericResponseWrapper, GenericValidationError } from '../../../../generic-responses';
 
@@ -26,9 +31,38 @@ export class ChangecalendarsService {
         );
     }
 
-    public addChangeCalendar(changeCalendar: AddChangeCalendar): Observable<GenericResponseWrapper> {
+    public getEdit(id: number): Observable<EditChangecalendarRoot> {
+        return this.http.get<EditChangecalendarRoot>(`${this.proxyPath}/changecalendar_module/changecalendars/edit/${id}.json?angular=true`)
+            .pipe(
+                map((data: EditChangecalendarRoot) => {
+                    return data;
+                })
+            );
+    }
+
+    public updateChangecalendar(changecalendar: EditChangecalendarRoot): Observable<GenericResponseWrapper> {
         const proxyPath: string = this.proxyPath;
-        return this.http.post<any>(`${proxyPath}/changecalendar_module/changecalendars/add.json?angular=true`, changeCalendar)
+        return this.http.post<any>(`${proxyPath}/changecalendar_module/changecalendars/edit/${changecalendar.changeCalendar.id}.json?angular=true`, changecalendar)
+            .pipe(
+                map(data => {
+                    return {
+                        success: true,
+                        data: data.changeCalendar as GenericIdResponse
+                    };
+                }),
+                catchError((error: any) => {
+                    const err = error.error.error as GenericValidationError;
+                    return of({
+                        success: false,
+                        data: err
+                    });
+                })
+            );
+
+    }
+
+    public addChangeCalendar(changeCalendar: AddChangeCalendar): Observable<GenericResponseWrapper> {
+        return this.http.post<any>(`${this.proxyPath}/changecalendar_module/changecalendars/add.json?angular=true`, changeCalendar)
             .pipe(
                 map(data => {
                     return {
