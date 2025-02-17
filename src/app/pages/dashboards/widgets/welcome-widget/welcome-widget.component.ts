39a88dc987d9014b09585aa4609b25ddfad326f8
nook24 <info@nook24.eu>
2025-02-17 11:26:20 +0100

    ITC-3206 Implement Welcome Widget

--- src/app/pages/dashboards/widgets/welcome-widget/welcome-widget.component.ts
+++ src/app/pages/dashboards/widgets/welcome-widget/welcome-widget.component.ts
@@ -1,18 +1,42 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { BaseWidgetComponent } from '../base-widget/base-widget.component';
 import { KtdGridLayout, KtdResizeEnd } from '@katoid/angular-grid-layout';
+import { forkJoin } from 'rxjs';
+import { WelcomeWidgetResponse } from '../widgets.interface';
+import { StatuscountResponse } from '../../../browsers/browsers.interface';
+import { TranslocoDirective } from '@jsverse/transloco';
+import { AsyncPipe } from '@angular/common';
+import { SystemnameService } from '../../../../services/systemname.service';
 
 @Component({
     selector: 'oitc-welcome-widget',
-    imports: [],
+    imports: [
+        TranslocoDirective,
+        AsyncPipe
+    ],
     templateUrl: './welcome-widget.component.html',
     styleUrl: './welcome-widget.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WelcomeWidgetComponent extends BaseWidgetComponent {
 
+    public welcome?: WelcomeWidgetResponse;
+    public statuscount?: StatuscountResponse;
+
+    public readonly SystemnameService = inject(SystemnameService);
+
     public override load() {
-        super.load();
+        let request = {
+            statuscount: this.WidgetsService.loadStatusCount(),
+            welcome: this.WidgetsService.loadWelcomeWidget()
+        };
+
+        forkJoin(request).subscribe(
+            (result) => {
+                this.statuscount = result.statuscount;
+                this.welcome = result.welcome;
+                this.cdr.markForCheck();
+            });
     }
 
     public override resizeWidget(event: KtdResizeEnd) {
---
+++ src/app/pages/dashboards/widgets/widgets.interface.ts
@@ -0,0 +1,9 @@
+export interface WelcomeWidgetResponse {
+    isCommunityEdition: boolean
+    hasSubscription: boolean
+    server_timezone: string
+    user_timezone: string
+    userImage: string
+    OPENITCOCKPIT_VERSION: string
+    _csrfToken: string
+}
