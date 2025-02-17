8b2003157797bfe345122556f7d6ae6e3eae5be0
ibering <irina.bering@it-novum.com>
2025-02-17 15:08:16 +0100

    #ITC-3206 Core: Dashboards (parent outages widget added)

--- src/app/pages/dashboards/widgets/parent-outages-widget/parent-outages-widget.component.ts
+++ src/app/pages/dashboards/widgets/parent-outages-widget/parent-outages-widget.component.ts
@@ -2,14 +2,19 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { BaseWidgetComponent } from '../base-widget/base-widget.component';
 import { DebounceDirective } from '../../../../directives/debounce.directive';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { FormControlDirective, InputGroupComponent, InputGroupTextDirective } from '@coreui/angular';
+import {
+    ColComponent,
+    FormControlDirective,
+    InputGroupComponent,
+    InputGroupTextDirective,
+    RowComponent
+} from '@coreui/angular';
 import { FormsModule } from '@angular/forms';
 import { TranslocoDirective, TranslocoPipe } from '@jsverse/transloco';
-import { getDefaultHostsIndexFilter, HostsIndexFilter } from '../../../hosts/hosts.interface';
 import { DashboardsService } from '../../dashboards.service';
 import { ParentOutage } from '../../dashboards.interface';
 import { HoststatusIconComponent } from '../../../hosts/hoststatus-icon/hoststatus-icon.component';
-import { NgForOf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
 import { LabelLinkComponent } from '../../../../layouts/coreui/label-link/label-link.component';
 
 @Component({
@@ -25,20 +30,23 @@ import { LabelLinkComponent } from '../../../../layouts/coreui/label-link/label-
         TranslocoDirective,
         HoststatusIconComponent,
         NgForOf,
-        LabelLinkComponent
+        LabelLinkComponent,
+        NgIf,
+        RowComponent,
+        ColComponent
     ],
     templateUrl: './parent-outages-widget.component.html',
     styleUrl: './parent-outages-widget.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ParentOutagesWidgetComponent extends BaseWidgetComponent {
-    public filter: HostsIndexFilter = getDefaultHostsIndexFilter();
+    public hostname: string = '';
     public readonly DashboardsService: DashboardsService = inject(DashboardsService);
     public parentOutages: ParentOutage[] = [];
 
     public override load() {
         if (this.widget) {
-            this.subscriptions.add(this.DashboardsService.getParentOutagesWidget(this.filter['Hosts.name'])
+            this.subscriptions.add(this.DashboardsService.getParentOutagesWidget(this.hostname)
                 .subscribe((result) => {
                     this.parentOutages = result.parent_outages;
                     this.cdr.markForCheck();
@@ -48,6 +56,7 @@ export class ParentOutagesWidgetComponent extends BaseWidgetComponent {
     }
 
     public onFilterChange($event: any) {
+        this.cdr.markForCheck();
         this.load();
     }
 }
