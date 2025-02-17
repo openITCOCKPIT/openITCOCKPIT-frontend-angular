c1357ea7e35a7bdd1566d4040ccf686fe3b45ef1
nook24 <info@nook24.eu>
2025-02-17 10:31:08 +0100

    ITC-3206 Create abstract base widget

---
+++ src/app/pages/dashboards/widgets/base-widget/base-widget.component.ts
@@ -0,0 +1,62 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, effect, inject, input, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { WidgetGetForRender } from '../../dashboards.interface';
+import { WidgetsService } from '../widgets.service';
+import { KtdGridLayout, KtdResizeEnd } from '@katoid/angular-grid-layout';
+
+@Component({
+    selector: 'oitc-base-widget',
+    imports: [],
+    templateUrl: './base-widget.component.html',
+    styleUrl: './base-widget.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export abstract class BaseWidgetComponent implements OnDestroy {
+
+    public widgetInput = input<WidgetGetForRender>();
+    public widget?: WidgetGetForRender;
+    public isReadonly = input<boolean>(false);
+
+    protected readonly subscriptions = new Subscription();
+    protected readonly cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
+    protected readonly WidgetsService = inject(WidgetsService);
+
+
+    public constructor() {
+        effect(() => {
+            this.widget = this.widgetInput();
+            this.load();
+        });
+
+        this.subscriptions.add(this.WidgetsService.onResizeEnded$.subscribe(event => {
+            if (this.widget) {
+                if (event.layoutItem.id === this.widget.id) {
+                    this.resizeWidget(event);
+                }
+            }
+        }));
+
+        this.subscriptions.add(this.WidgetsService.onLayoutUpdated$.subscribe(event => {
+            if (this.widget) {
+                this.layoutUpdate(event);
+            }
+        }));
+
+    }
+
+    public load() {
+        console.log('Implement load() method in your widget component');
+    }
+
+    public ngOnDestroy() {
+        this.subscriptions.unsubscribe();
+    }
+
+    public resizeWidget(event: KtdResizeEnd) {
+        console.log('Implement resizeWidget(event: KtdResizeEnd) method in your widget component');
+    }
+
+    public layoutUpdate(event: KtdGridLayout) {
+        console.log('Implement layoutUpdate(event: KtdGridLayout) method in your widget component');
+    }
+}
