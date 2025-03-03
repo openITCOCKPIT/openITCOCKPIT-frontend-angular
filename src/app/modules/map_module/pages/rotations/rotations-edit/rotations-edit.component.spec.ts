74b8db1c9b6984e5daf4dfaca6c4d8ecee633491
abergen <alex.bergen@it-novum.com>
2025-03-03 08:33:55 +0000

    ITC-3270 - fixed typo

--- src/app/modules/map_module/pages/rotations/rotations-edit/rotations-edit.component.spec.ts
+++ src/app/modules/map_module/pages/rotations/rotations-edit/rotations-edit.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RotationsEditComponent } from './rotations-edit.component';
 
-describe('MapsEditComponent', () => {
+describe('RotationsEditComponent', () => {
     let component: RotationsEditComponent;
     let fixture: ComponentFixture<RotationsEditComponent>;
 
