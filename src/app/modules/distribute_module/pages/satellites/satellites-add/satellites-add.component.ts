24668fa10c0793170cb45d3f3fb5c31e8f34c089
Jan Nox <jan.nox@pm.me>
2025-03-04 14:34:53 +0000

    ITC-3288 * Satellites index

---
+++ src/app/modules/distribute_module/pages/satellites/satellites-add/satellites-add.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'oitc-satellites-add',
+  imports: [],
+  templateUrl: './satellites-add.component.html',
+  styleUrl: './satellites-add.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SatellitesAddComponent {
+
+}
