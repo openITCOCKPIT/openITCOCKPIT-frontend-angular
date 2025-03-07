7a8f0e7f0214a605b4aa9e97f3869aa48021d336
Dietmar Jenderny <dietmar.jenderny@it-novum.com>
2025-03-07 13:08:04 +0100

    usedBy in delete modal now complete

---
+++ src/app/layouts/coreui/coreui-header/header-time/header-time.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'oitc-header-time',
+  imports: [],
+  templateUrl: './header-time.component.html',
+  styleUrl: './header-time.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderTimeComponent {
+
+}
