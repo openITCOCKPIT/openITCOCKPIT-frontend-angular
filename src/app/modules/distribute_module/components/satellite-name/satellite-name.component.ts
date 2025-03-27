c3d8d465f804072d832da05164c8b9553eb6ee85
nook24 <daniel.ziegler@it-novum.com>
2025-03-27 15:42:02 +0100

    Catch error if a user has no permission to load the satellite name in the browser view of hosts (ROOT_CONTAINER or Sharing host)

--- src/app/modules/distribute_module/components/satellite-name/satellite-name.component.ts
+++ src/app/modules/distribute_module/components/satellite-name/satellite-name.component.ts
@@ -4,12 +4,16 @@ import { Subscription } from 'rxjs';
 import { SatellitesService } from '../../pages/satellites/satellites.service';
 import { SatelliteEntityCake2 } from '../../pages/satellites/satellites.interface';
 import { SkeletonModule } from 'primeng/skeleton';
+import { TranslocoDirective } from '@jsverse/transloco';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 @Component({
     selector: 'oitc-satellite-name',
     imports: [
         NgIf,
-        SkeletonModule
+        SkeletonModule,
+        TranslocoDirective,
+        FaIconComponent
     ],
     templateUrl: './satellite-name.component.html',
     styleUrl: './satellite-name.component.css',
@@ -20,6 +24,7 @@ export class SatelliteNameComponent implements OnDestroy {
     public satelliteId = input<number>(0);
 
     public satellite?: SatelliteEntityCake2;
+    public restrictedSatellite: boolean = false;
 
     private subscriptions: Subscription = new Subscription();
     private readonly SatellitesService = inject(SatellitesService);
@@ -38,7 +43,18 @@ export class SatelliteNameComponent implements OnDestroy {
     public load() {
         this.subscriptions.add(
             this.SatellitesService.getSatelliteById(this.satelliteId()).subscribe((data) => {
-                this.satellite = data;
+                if (!data.hasOwnProperty('error') && !data.hasOwnProperty('status')) {
+                    this.satellite = data as SatelliteEntityCake2;
+                }
+
+                if (data.hasOwnProperty('error') && data.hasOwnProperty('status')) {
+                    if (data.status === 403) {
+                        // We have no permission to view this satellite
+                        // Probably we can only see this host through host sharing, or it is in the ROOT_CONTAINER
+                        this.restrictedSatellite = true;
+                    }
+                }
+
                 this.cdr.markForCheck();
             })
         );
