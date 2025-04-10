79b6c5b1636a227838de0ff5e22c4797faf21518
ibering <irina.bering@it-novum.com>
2025-04-10 12:01:08 +0200

    Eventcorrelation widget: config for type and direction added

--- src/app/modules/eventcorrelation_module/widgets/eventcorrelation-widget/eventcorrelation-widget.interface.ts
+++ src/app/modules/eventcorrelation_module/widgets/eventcorrelation-widget/eventcorrelation-widget.interface.ts
@@ -1,4 +1,10 @@
 export interface EventcorrelationWidgetConfigRootResponse {
     host_id: number | null
     _csrfToken: string | null
+    config: EventcorrelationWidgetConfig
+}
+
+export interface EventcorrelationWidgetConfig{
+    type: 'tree' | 'table'
+    direction: 'LEFT_TO_RIGHT' | 'TOP_TO_BOTTOM' | 'RIGHT_TO_LEFT' | 'BOTTOM_TO_TOP'
 }
