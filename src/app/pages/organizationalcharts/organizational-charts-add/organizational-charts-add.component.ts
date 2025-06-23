effc3abc8f81a80d48f79239fcae96248b4f7511
nook24 <daniel.ziegler@it-novum.com>
2025-06-23 13:08:32 +0200

    ITC-3435 Basic add layout

--- src/app/pages/organizationalcharts/organizational-charts-add/organizational-charts-add.component.ts
+++ src/app/pages/organizationalcharts/organizational-charts-add/organizational-charts-add.component.ts
@@ -4,6 +4,9 @@ import {
     OrganizationalChartsEditorComponent
 } from '../organizational-charts-editor/organizational-charts-editor.component';
 import { TranslocoDirective } from '@jsverse/transloco';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { PermissionDirective } from '../../../permissions/permission.directive';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'oitc-organizational-charts-add',
@@ -13,7 +16,10 @@ import { TranslocoDirective } from '@jsverse/transloco';
         CardHeaderComponent,
         CardTitleDirective,
         OrganizationalChartsEditorComponent,
-        TranslocoDirective
+        TranslocoDirective,
+        FaIconComponent,
+        PermissionDirective,
+        RouterLink
     ],
     templateUrl: './organizational-charts-add.component.html',
     styleUrl: './organizational-charts-add.component.css',
