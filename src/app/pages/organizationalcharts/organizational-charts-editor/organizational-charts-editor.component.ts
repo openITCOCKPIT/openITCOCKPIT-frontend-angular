bb79c599bd2dbadf599bd14c3f332cbe42168165
nook24 <daniel.ziegler@it-novum.com>
2025-06-26 15:02:59 +0200

    ITC-3435 Implement update of nodes Implement connection handling for drag&drop and keyboard shortcuts

--- src/app/pages/organizationalcharts/organizational-charts-editor/organizational-charts-editor.component.ts
+++ src/app/pages/organizationalcharts/organizational-charts-editor/organizational-charts-editor.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, model, OnInit, ViewChild } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    HostListener,
+    model,
+    OnInit,
+    ViewChild
+} from '@angular/core';
 import { NodesPaletteComponent } from './nodes-palette/nodes-palette.component';
 import {
     EFConnectionBehavior,
@@ -31,7 +39,7 @@ import { NgClass } from '@angular/common';
     ],
     templateUrl: './organizational-charts-editor.component.html',
     styleUrl: './organizational-charts-editor.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OrganizationalChartsEditorComponent implements OnInit {
 
@@ -41,11 +49,39 @@ export class OrganizationalChartsEditorComponent implements OnInit {
     public nodes: OcTreeNode[] = [];
     public connections: OrganizationalChartsTreeConnection[] = [];
 
+    @ViewChild(FFlowComponent, {static: false})
+    public fFlowComponent!: FFlowComponent;
+
     @ViewChild(FCanvasComponent, {static: true})
     public fCanvasComponent!: FCanvasComponent;
 
     @ViewChild(FZoomDirective, {static: true})
     public fZoomDirective!: FZoomDirective;
+
+    @HostListener('window:keydown', ['$event'])
+    onKeydown(event: KeyboardEvent): void {
+        console.log('Keydown event:', event);
+        if (event.target instanceof HTMLInputElement || event.target instanceof HTMLTextAreaElement) {
+            return;
+        }
+
+        console.log('Key pressed:', event.key, 'KeyCode:', event.keyCode);
+
+        switch (event.key) {
+            case 'Backspace':
+            case 'Delete':
+                console.log('Backspace pressed');
+
+                const selection = this.fFlowComponent.getSelection();
+
+                // Are there any connections selected? If so, remove them
+                selection.fConnectionIds.forEach((connectionId) => {
+                    this.onRemoveConnection(connectionId);
+                });
+
+                break;
+        }
+    }
 
     protected readonly eMarkerType = EFMarkerType;
 
@@ -97,21 +133,10 @@ export class OrganizationalChartsEditorComponent implements OnInit {
         });
 
         // Update two-way binding
-        this.nodeTree.update((nodes) => {
-            return [...nodes, newNode];
-        });
-
-        this.cdr.markForCheck();
+        this.addNodeToTree(newNode);
 
 
         //this.getData();
-    }
-
-    public onReassignConnection(event: FReassignConnectionEvent): void {
-        console.log('onReassignConnection');
-        console.log(event);
-        //    this.apiService.reassignConnection(event.fOutputId, event.oldFInputId, event.newFInputId);
-        this.getData();
     }
 
     public onConnectionAdded(event: FCreateConnectionEvent): void {
@@ -129,22 +154,117 @@ export class OrganizationalChartsEditorComponent implements OnInit {
         };
 
         this.connections = [...this.connections, newConnection];
+
+        // Update the two-way binding
+        const node = this.nodeTree().find((n) => n.id === event.fInputId);
+        if (!node) {
+            console.log('Node not found for connection input:', event.fInputId);
+            return;
+        }
+
+        node.parent_id = event.fOutputId;
+        this.updateNodeInTree(node);
+
         this.cdr.markForCheck();
     }
 
-    public onNodePositionChanged(point: IPoint, node: any): void {
+    public onReassignConnection(event: FReassignConnectionEvent): void {
+        if (!event.newFInputId) {
+            // newFInputId undefined means that the connection has been reassigned to the same input/node
+            // nothing to do in this case
+            return;
+        }
+
+        // Delete old connection from f-flow canvas and add new one
+        this.connections = this.connections.filter((c) => c.uuid !== event.fConnectionId);
+
+        // So we can track connections in @for loop
+        const uuid = new UUID();
+        const newConnection: OrganizationalChartsTreeConnection = {
+            uuid: uuid.v4(),
+            fInputId: event.newFInputId,
+            fOutputId: event.fOutputId
+        };
+
+        this.connections = [...this.connections, newConnection];
+
+        // Remove the parent_id from the old node
+        const oldNode = this.nodeTree().find((n) => n.id === event.oldFInputId);
+        if (!oldNode) {
+            console.log('Old node not found for connection reassignment:', event.oldFInputId);
+            return;
+        }
+
+        oldNode.parent_id = null;
+        this.updateNodeInTree(oldNode);
+
+        // Update the parent_id of the new node
+        const newNode = this.nodeTree().find((n) => n.id === event.newFInputId);
+        if (!newNode) {
+            console.log('New node not found for connection reassignment:', event.newFInputId);
+            return;
+        }
+
+        newNode.parent_id = event.fOutputId;
+        this.updateNodeInTree(newNode);
+    }
+
+    public onRemoveConnection(connectionId: string) {
+        const connection = this.connections.find((c) => c.uuid === connectionId);
+        if (!connection) {
+            console.log('Connection not found:', connectionId);
+            return;
+        }
+
+        // Remove the connection from the f-flow canvas
+        this.connections = this.connections.filter((c) => c.uuid !== connectionId);
+
+        // Find the node that had this connection as input and remove the parent_id
+        const node = this.nodeTree().find((n) => n.id === connection.fInputId);
+        if (node) {
+            node.parent_id = null;
+            this.updateNodeInTree(node);
+        }
+    }
+
+    public onNodePositionChanged(point: IPoint, node: OcTreeNode): void {
         node.position = point;
 
         const x = Math.floor(node.position.x);
         const y = Math.floor(node.position.y);
 
-        console.log('onNodePositionChanged');
-        console.log(point);
-        console.log(node);
+        node.node.x_position = x;
+        node.node.y_position = y;
 
-        //this.apiService.moveNode(node.id, point);
+        this.updateNodeInTree(node.node);
+    }
+
+    private addNodeToTree(node: OrganizationalChartsTreeNode): void {
+        this.nodeTree.update((nodes) => {
+            return [...nodes, node];
+        });
+
+        this.cdr.markForCheck();
+    }
+
+    private updateNodeInTree(node: OrganizationalChartsTreeNode): void {
+        this.nodeTree.update((nodes) => {
+            console.log(nodes.map((n) => n.id === node.id ? node : n));
+            return nodes.map((n) => n.id === node.id ? node : n);
+        });
+
+        this.cdr.markForCheck();
+    }
+
+    private removeNodeFromTree(node: OrganizationalChartsTreeNode): void {
+        this.nodeTree.update((nodes) => {
+            return nodes.filter((n) => n.id !== node.id);
+        });
+
+        this.cdr.markForCheck();
     }
 
     protected readonly EFConnectionBehavior = EFConnectionBehavior;
     protected readonly EFConnectionType = EFConnectionType;
+    protected readonly String = String;
 }
--- src/app/pages/organizationalcharts/organizational-charts-editor/tenant-node/tenant-node.component.ts
+++ src/app/pages/organizationalcharts/organizational-charts-editor/tenant-node/tenant-node.component.ts
@@ -1,14 +1,17 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, input, Input, output } from '@angular/core';
 import { OcTreeNode } from '../organizational-charts-editor.interface';
 import { EFConnectableSide, FFlowModule } from '@foblex/flow';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { JsonPipe } from '@angular/common';
+import { OrganizationalChartsTreeConnection } from '../../organizationalcharts.interface';
 
 
 @Component({
     selector: 'oitc-tenant-node',
     imports: [
         FFlowModule,
-        FaIconComponent
+        FaIconComponent,
+        JsonPipe
     ],
     templateUrl: './tenant-node.component.html',
     styleUrl: './tenant-node.component.scss',
@@ -17,5 +20,36 @@ import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 export class TenantNodeComponent {
 
     @Input() public node: OcTreeNode | undefined;
+
+    public connectionsInput = input<OrganizationalChartsTreeConnection[]>([]);
+    public deleteConnection = output<string>();
+
     protected readonly EFConnectableSide = EFConnectableSide;
+    protected readonly String = String;
+
+    public connections: OrganizationalChartsTreeConnection[] = [];
+    public hasConnection: boolean = false;
+
+    private connectionUuid: string = '';
+
+    public constructor() {
+        effect(() => {
+            this.connections = this.connectionsInput();
+
+            // Check if the node has a connection
+            this.hasConnection = false;
+            const connection = this.connections.find((c) => c.fOutputId === this.node?.node.id);
+            if (connection) {
+                this.hasConnection = true;
+                this.connectionUuid = connection.uuid;
+            }
+        });
+    }
+
+    public onDeleteConnection(): void {
+        if (this.hasConnection && this.connectionUuid !== '') {
+            this.deleteConnection.emit(this.connectionUuid);
+        }
+    }
+
 }
