72012bd64ec96543c6df1ddcc18198abb2b2cd87
Jan Nox <jan.nox@pm.me>
2025-07-03 06:33:17 +0000

    * Watch for the design Module. * If present, add custom CSS file to the page. * Also, add the timestamp to it to simplify refreshing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import {
     Component,
     inject,
     OnDestroy,
+    OnInit,
+    Renderer2,
     signal
 } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
@@ -34,6 +36,7 @@ import { MessagesOfTheDayService } from './pages/messagesotd/messagesotd.service
 import { AuthService } from './auth/auth.service';
 import { TitleService } from './services/title.service';
 import { SystemnameService } from './services/systemname.service';
+import { PermissionsService } from './permissions/permissions.service';
 
 @Component({
     selector: 'oitc-root',
@@ -54,7 +57,7 @@ import { SystemnameService } from './services/systemname.service';
     styleUrl: './app.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnDestroy, AfterViewInit {
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
 
     // Inject HistoryService to keep track of the previous URLs
     private historyService: HistoryService = inject(HistoryService);
@@ -62,9 +65,11 @@ export class AppComponent implements OnDestroy, AfterViewInit {
     // I am the current messageOfTheDay.
     protected messageOfTheDay: CurrentMessageOfTheDay = {} as CurrentMessageOfTheDay;
 
-    private readonly TitleService: TitleService = inject(TitleService);
     public readonly LayoutService = inject(LayoutService);
+    public readonly PermissionsService: PermissionsService = inject(PermissionsService);
+    private readonly TitleService: TitleService = inject(TitleService);
     private readonly document = inject(DOCUMENT);
+    private readonly renderer: Renderer2 = inject(Renderer2);
     private navigationEndEvent: NavigationEnd | null = null;
 
     protected systemName: string = '';
@@ -152,6 +157,23 @@ export class AppComponent implements OnDestroy, AfterViewInit {
                 this.cdr.markForCheck();
             }));
         }));
+    }
+
+    ngOnInit(): void {
+        this.appendCustomStyle();
+    }
+
+    private appendCustomStyle(): void {
+        this.PermissionsService.hasModuleObservable('DesignModule').subscribe(hasModule => {
+            if (!hasModule) {
+                return;
+            }
+            const link = this.renderer.createElement('link');
+            link.rel = 'stylesheet';
+            link.type = 'text/css';
+            link.href = `https://master/design_module/css/customStyle.css?v=${new Date().getTime()}`;
+            this.renderer.appendChild(document.head, link);
+        });
     }
 
     public ngOnDestroy(): void {
