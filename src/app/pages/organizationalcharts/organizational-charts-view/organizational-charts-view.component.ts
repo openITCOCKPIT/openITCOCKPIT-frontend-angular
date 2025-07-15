b22eb52479d862bf643f665529ad2ac950e93eb7
ibering <i.bering@allgeier-its.com>
2025-07-15 16:25:11 +0200

    #ITC-3435 Organizational chart - Containers:  Alternative tree view [Browser] - view page added (initial commit)

---
+++ src/app/pages/organizationalcharts/organizational-charts-view/organizational-charts-view.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'oitc-organizational-charts-view',
+  imports: [],
+  templateUrl: './organizational-charts-view.component.html',
+  styleUrl: './organizational-charts-view.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class OrganizationalChartsViewComponent {
+
+}
