d857e6f6ebb20fd5c5244a198a35601a8521317d
ibering <i.bering@allgeier-its.com>
2025-08-25 16:22:50 +0200

    #ITC-3457 ServiceCapacityManagementModule - Send Status Email to Mailing List: index view (first steps)

--- src/app/modules/scm_module/pages/mailinglists/mailinglists.interface.ts
+++ src/app/modules/scm_module/pages/mailinglists/mailinglists.interface.ts
@@ -1,2 +1,40 @@
-export interface MailinglistsInterface {
+import { PaginateOrScroll } from '../../../../layouts/coreui/paginator/paginator.interface';
+
+export interface MailinglistsIndex extends PaginateOrScroll {
+    all_mailinglists: Mailinglist[]
+    _csrfToken: string
+}
+
+
+export interface Mailinglist {
+    id: number
+    container_id: number
+    name: string
+    description: string
+    department: string
+    allow_edit: boolean
+}
+
+export interface MailinglistsIndexParams {
+    angular: true,
+    scroll: boolean,
+    sort: string,
+    page: number,
+    direction: 'asc' | 'desc' | '', // asc or desc
+    'filter[Mailinglists.id][]': number[],
+    'filter[Mailinglists.name]': string
+    'filter[Mailinglists.description]': string
+}
+
+export function getMailinglistsIndexParams(): MailinglistsIndexParams {
+    return {
+        angular: true,
+        scroll: true,
+        sort: 'Mailinglists.name',
+        page: 1,
+        direction: 'asc',
+        'filter[Mailinglists.id][]': [],
+        'filter[Mailinglists.name]': '',
+        'filter[Mailinglists.description]': ''
+    }
 }
--- src/app/modules/scm_module/pages/mailinglists/mailinglists.service.ts
+++ src/app/modules/scm_module/pages/mailinglists/mailinglists.service.ts
@@ -1,8 +1,35 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { ResourcegroupsIndex, ResourcegroupsIndexParams } from '../resourcegroups/resourcegroups.interface';
+import { map, Observable } from 'rxjs';
+import { MailinglistsIndex, MailinglistsIndexParams } from './mailinglists.interface';
+import { HttpClient } from '@angular/common/http';
+import { PROXY_PATH } from '../../../../tokens/proxy-path.token';
+import { DeleteAllItem } from '../../../../layouts/coreui/delete-all-modal/delete-all.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MailinglistsService {
-  
+    private readonly http = inject(HttpClient);
+    private readonly proxyPath = inject(PROXY_PATH);
+    public getMailinglists(params: MailinglistsIndexParams): Observable<MailinglistsIndex> {
+        const proxyPath = this.proxyPath;
+        return this.http.get<MailinglistsIndex>(`${proxyPath}/scm_module/mailinglists/index.json`, {
+            params: params as {} // cast MailinglistsIndexParams into object
+        }).pipe(
+            map(data => {
+                return data;
+            })
+        )
+    }
+
+    /**********************
+     *    Delete action    *
+     **********************/
+    // Generic function for the Delete All Modal
+    public delete(item: DeleteAllItem): Observable<Object> {
+        const proxyPath = this.proxyPath;
+        return this.http.post(`${proxyPath}/scm_module/mailinglists/delete/${item.id}.json?angular=true`, {});
+    }
+
 }
