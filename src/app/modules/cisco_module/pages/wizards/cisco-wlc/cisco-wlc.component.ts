7bfb7938611105b0e463c1d927d7b808ff77e5e6
abergen <a.bergen@allgeier-its.com>
2025-10-28 14:40:35 +0000

    ITC-3553 - removed unused code

--- src/app/modules/cisco_module/pages/wizards/cisco-wlc/cisco-wlc.component.ts
+++ src/app/modules/cisco_module/pages/wizards/cisco-wlc/cisco-wlc.component.ts
@@ -1,12 +1,11 @@
-import { ChangeDetectionStrategy, Component, inject, ViewChild, ViewChildren } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, ViewChild } from '@angular/core';
 import { WizardsAbstractComponent } from '../../../../../pages/wizards/wizards-abstract/wizards-abstract.component';
 import { SelectKeyValueString } from '../../../../../layouts/primeng/select.interface';
 import { CiscoWlcWizardService } from './cisco-wlc-wizard.service';
-import { CiscoWlcWizardGet, CiscoWlcWizardPost, InterfaceServicetemplate, N0 } from './cisco-wlc-wizard.interface';
+import { CiscoWlcWizardPost } from './cisco-wlc-wizard.interface';
 import { RouterLink } from '@angular/router';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import {
-    AccordionItemComponent,
     CardBodyComponent,
     CardComponent,
     CardHeaderComponent,
@@ -25,8 +24,6 @@ import {
     WizardsDynamicfieldsComponent
 } from '../../../../../components/wizards/wizards-dynamicfields/wizards-dynamicfields.component';
 import { ProgressBarModule } from 'primeng/progressbar';
-import { GenericResponseWrapper, GenericValidationError } from '../../../../../generic-responses';
-import { Service } from '../../../../../pages/wizards/wizards.interface';
 import { BackButtonDirective } from '../../../../../directives/back-button.directive';
 
 @Component({
@@ -57,7 +54,6 @@ import { BackButtonDirective } from '../../../../../directives/back-button.direc
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CiscoWlcComponent extends WizardsAbstractComponent {
-    @ViewChildren('accordionItem') accordionItems: AccordionItemComponent[] = [];
     @ViewChild(WizardsDynamicfieldsComponent) childComponentLocal!: WizardsDynamicfieldsComponent;
     protected override WizardService: CiscoWlcWizardService = inject(CiscoWlcWizardService);
     public checked: boolean = false;
@@ -102,90 +98,5 @@ export class CiscoWlcComponent extends WizardsAbstractComponent {
         {key: '3DES', value: '3des'},
         {key: '3DESDE', value: '3desde'},
     ];
-    protected interfaceServicetemplate: InterfaceServicetemplate = {} as InterfaceServicetemplate;
-
-    protected override wizardLoad(result: CiscoWlcWizardGet): void {
-        this.interfaceServicetemplate = result.interfaceServicetemplate;
-
-        super.wizardLoad(result);
-    }
-
-    public override submit(): void {
-        // let request = this.post; // Clone the original post object here!
-        let request: CiscoWlcWizardPost = JSON.parse(JSON.stringify(this.post));
-
-        // Remove all services from request where createService is false.
-        request.services = request.services.filter((service: Service) => {
-            return service.createService && this.childComponent.hasName(service.name);
-        });
-        // Remove all interfaces from request where createService is false.
-        request.interfaces = [];
-
-        this.subscriptions.add(this.WizardService.submit(request)
-            .subscribe((result: GenericResponseWrapper) => {
-                this.errors = {} as GenericValidationError;
-                if (result.success) {
-                    const title: string = this.TranslocoService.translate('Success');
-                    const msg: string = this.TranslocoService.translate('Data saved successfully');
-
-                    this.notyService.genericSuccess(msg, title);
-                    this.router.navigate(['/services/notMonitored']);
-                    this.cdr.markForCheck();
-                    return;
-                }
-                // Error
-                this.notyService.genericError();
-                this.notyService.scrollContentDivToTop();
-                const errorResponse: GenericValidationError = result.data as GenericValidationError;
-                if (result) {
-                    this.errors = errorResponse;
-
-                }
-                this.cdr.markForCheck();
-            })
-        );
-    }
-
-    protected toggleCheck(checked: boolean): void {
-        this.checked = checked;
-        this.post.interfaces.forEach((service: N0) => {
-            if (!this.hasName(service.name)) {
-                return;
-            }
-            service.createService = this.checked;
-        });
-        this.cdr.markForCheck();
-    }
-
-    protected toggleAccordionClose(checked: boolean): void {
-        this.accordionClosed = checked;
-        this.accordionItems.forEach((accordionItem: AccordionItemComponent) => {
-            if ((accordionItem.visible && this.accordionClosed) || (!accordionItem.visible && !this.accordionClosed)) {
-                accordionItem.toggleItem();
-            }
-        });
-        this.cdr.markForCheck();
-    }
-
-    protected detectColor = function (label: string): string {
-        if (label.match(/warning/gi)) {
-            return 'warning';
-        }
-
-        if (label.match(/critical/gi)) {
-            return 'critical';
-        }
-
-        return '';
-    };
-
-    protected hasName = (name: string): boolean => {
-        if (this.searchedTags.length === 0) {
-            return true;
-        }
-        return this.searchedTags.some((tag) => {
-            return name.toLowerCase().includes(tag.toLowerCase());
-        });
-    }
 
 }
