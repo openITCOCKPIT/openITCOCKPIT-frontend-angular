a5f7259aa3caf33403e47f91375c32cf11caca2e
abergen <a.bergen@allgeier-its.com>
2025-10-29 15:14:10 +0000

    ITC-3567 - fixed typos and removed unused code

--- src/app/modules/mshyperv_module/pages/wizards/hyper-v/hyper-v-wizard.interface.ts
+++ src/app/modules/mshyperv_module/pages/wizards/hyper-v/hyper-v-wizard.interface.ts
@@ -7,27 +7,6 @@ export interface HyperVWizardGet extends WizardGet {
     winRmAuthMode: string
 }
 
-
-export interface Servicecommandargumentvalue {
-    commandargument: Commandargument
-    commandargument_id: number
-    created: string
-    id: number
-    modified: string
-    servicetemplate_id: number
-    value: string
-}
-
-export interface Commandargument {
-    command_id: number
-    created: string
-    human_name: string
-    id: number
-    modified: string
-    name: string
-}
-
-
 // WIZARD POST
 export interface HyperVWizardPost extends WizardPost {
     winRmUser: string
