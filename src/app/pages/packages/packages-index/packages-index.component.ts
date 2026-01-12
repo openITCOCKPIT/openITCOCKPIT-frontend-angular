80739be0a2dc77eb4e578795551777e5fce63687
ibering <i.bering@allgeier-its.com>
2026-01-12 11:13:27 +0100

    ITC-3676 Software inventory and patch management (initial commit)

---
+++ src/app/pages/packages/packages-index/packages-index.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'oitc-packages-index',
+  imports: [],
+  templateUrl: './packages-index.component.html',
+  styleUrl: './packages-index.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PackagesIndexComponent {
+
+}
