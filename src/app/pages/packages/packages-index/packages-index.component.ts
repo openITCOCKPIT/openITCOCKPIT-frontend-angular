54339f6587a013d6d2a72d18b8ac3fefdb6e8716
ibering <i.bering@allgeier-its.com>
2026-01-20 09:23:08 +0100

    ITC-3676 Software inventory and patch management (index view with dummy data)

--- src/app/pages/packages/packages-index/packages-index.component.ts
+++ src/app/pages/packages/packages-index/packages-index.component.ts
@@ -1,12 +1,53 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { PermissionDirective } from '../../../permissions/permission.directive';
+import { TranslocoDirective, TranslocoPipe } from '@jsverse/transloco';
+import { RouterLink } from '@angular/router';
+import {
+    CardBodyComponent,
+    CardComponent,
+    CardHeaderComponent,
+    CardTitleDirective,
+    ColComponent,
+    RowComponent, TabDirective, TableDirective,
+    TabPanelComponent, TabsComponent, TabsContentComponent, TabsListComponent,
+    TemplateIdDirective,
+    TextColorDirective,
+    WidgetStatFComponent
+} from '@coreui/angular';
+
+import { cil3d, cilList, cilShieldAlt } from '@coreui/icons';
+import { BadgeOutlineComponent } from '../../../layouts/coreui/badge-outline/badge-outline.component';
 
 @Component({
-  selector: 'oitc-packages-index',
-  imports: [],
-  templateUrl: './packages-index.component.html',
-  styleUrl: './packages-index.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'oitc-packages-index',
+    imports: [
+        FaIconComponent,
+        PermissionDirective,
+        TranslocoDirective,
+        RouterLink,
+        CardBodyComponent,
+        CardComponent,
+        CardHeaderComponent,
+        CardTitleDirective,
+        ColComponent,
+        RowComponent,
+        TemplateIdDirective,
+        WidgetStatFComponent,
+        TextColorDirective,
+        TranslocoPipe,
+        TableDirective,
+        BadgeOutlineComponent,
+        TabsComponent,
+        TabsListComponent,
+        TabDirective,
+        TabsContentComponent,
+        TabPanelComponent
+    ],
+    templateUrl: './packages-index.component.html',
+    styleUrl: './packages-index.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PackagesIndexComponent {
-
+    icons = {cilList, cilShieldAlt, cil3d};
 }
