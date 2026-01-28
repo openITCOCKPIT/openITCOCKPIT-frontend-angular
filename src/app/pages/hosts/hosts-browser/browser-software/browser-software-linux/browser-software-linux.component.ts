6587a4d659c4aa9c69fb1faa97caa43d5863f946
ibering <i.bering@allgeier-its.com>
2026-01-28 10:45:40 +0100

    ITC-3676 Software inventory and patch management (hosts browser view: browser-software-* components added (default methods added))

--- src/app/pages/hosts/hosts-browser/browser-software/browser-software-linux/browser-software-linux.component.ts
+++ src/app/pages/hosts/hosts-browser/browser-software/browser-software-linux/browser-software-linux.component.ts
@@ -1,12 +1,56 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, InputSignal, OnDestroy, OnInit } from '@angular/core';
+import { IndexPage } from '../../../../../pages.interface';
+import { Sort } from '@angular/material/sort';
+import { PaginatorChangeEvent } from '../../../../../layouts/coreui/paginator/paginator.interface';
+import { Subscription } from 'rxjs';
+import { TranslocoService } from '@jsverse/transloco';
 
 @Component({
-  selector: 'oitc-browser-software-linux',
-  imports: [],
-  templateUrl: './browser-software-linux.component.html',
-  styleUrl: './browser-software-linux.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'oitc-browser-software-linux',
+    imports: [],
+    templateUrl: './browser-software-linux.component.html',
+    styleUrl: './browser-software-linux.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BrowserSoftwareLinuxComponent {
+export class BrowserSoftwareLinuxComponent implements OnInit, OnDestroy, IndexPage {
+    public hostId: InputSignal<number> = input<number>(0);
 
+    private subscription: Subscription = new Subscription();
+    private readonly TranslocoService = inject(TranslocoService);
+
+    ngOnInit(): void {
+        this.load();
+    }
+
+    load(): void {
+        
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+
+    toggleFilter(): void {
+        throw new Error('Method not implemented.');
+    }
+
+    resetFilter(): void {
+        throw new Error('Method not implemented.');
+    }
+
+    onPaginatorChange(change: PaginatorChangeEvent): void {
+        throw new Error('Method not implemented.');
+    }
+
+    onFilterChange(event: Event): void {
+        throw new Error('Method not implemented.');
+    }
+
+    onSortChange(sort: Sort): void {
+        throw new Error('Method not implemented.');
+    }
+
+    onMassActionComplete(success: boolean): void {
+        throw new Error('Method not implemented.');
+    }
 }
--- src/app/pages/hosts/hosts-browser/browser-software/browser-software-windows/browser-software-windows.component.ts
+++ src/app/pages/hosts/hosts-browser/browser-software/browser-software-windows/browser-software-windows.component.ts
@@ -1,12 +1,53 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, InputSignal, OnDestroy, OnInit } from '@angular/core';
+import { IndexPage } from '../../../../../pages.interface';
+import { PaginatorChangeEvent } from '../../../../../layouts/coreui/paginator/paginator.interface';
+import { Sort } from '@angular/material/sort';
+import { Subscription } from 'rxjs';
+import { TranslocoService } from '@jsverse/transloco';
+
 
 @Component({
-  selector: 'oitc-browser-software-windows',
-  imports: [],
-  templateUrl: './browser-software-windows.component.html',
-  styleUrl: './browser-software-windows.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'oitc-browser-software-windows',
+    imports: [],
+    templateUrl: './browser-software-windows.component.html',
+    styleUrl: './browser-software-windows.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BrowserSoftwareWindowsComponent {
+export class BrowserSoftwareWindowsComponent implements OnInit, OnDestroy, IndexPage {
+    public hostId: InputSignal<number> = input<number>(0);
 
+    private subscription: Subscription = new Subscription();
+    private readonly TranslocoService = inject(TranslocoService);
+
+    ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+
+    toggleFilter(): void {
+        throw new Error('Method not implemented.');
+    }
+
+    resetFilter(): void {
+        throw new Error('Method not implemented.');
+    }
+
+    onPaginatorChange(change: PaginatorChangeEvent): void {
+        throw new Error('Method not implemented.');
+    }
+
+    onFilterChange(event: Event): void {
+        throw new Error('Method not implemented.');
+    }
+
+    onSortChange(sort: Sort): void {
+        throw new Error('Method not implemented.');
+    }
+
+    onMassActionComplete(success: boolean): void {
+        throw new Error('Method not implemented.');
+    }
 }
