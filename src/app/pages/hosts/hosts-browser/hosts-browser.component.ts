edbbe753381bde8e92d6400858e4a54f53029fa8
Jan Nox <jan@nox.kiwi>
2026-03-13 09:38:44 +0100

    ITC-3654 * Add the component on the TS file, also remove old stuff that is not needed any more.

--- src/app/pages/hosts/hosts-browser/hosts-browser.component.ts
+++ src/app/pages/hosts/hosts-browser/hosts-browser.component.ts
@@ -1,12 +1,4 @@
-import {
-    ChangeDetectionStrategy,
-    ChangeDetectorRef,
-    Component,
-    DOCUMENT,
-    inject,
-    OnDestroy,
-    OnInit
-} from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { TranslocoDirective, TranslocoPipe, TranslocoService } from '@jsverse/transloco';
 import {
     QueryHandlerCheckerComponent
@@ -40,7 +32,7 @@ import {
     TooltipDirective
 } from '@coreui/angular';
 import { HostBrowserMenuConfig, HostsBrowserMenuComponent } from '../hosts-browser-menu/hosts-browser-menu.component';
-import { AsyncPipe, JsonPipe, KeyValuePipe, NgClass } from '@angular/common';
+import { AsyncPipe, KeyValuePipe, NgClass } from '@angular/common';
 import { BrowserLoaderComponent } from '../../../layouts/primeng/loading/browser-loader/browser-loader.component';
 import { HostBrowserResult, HostBrowserSlaOverview, MergedHost, SoftwareInformationHost } from '../hosts.interface';
 import { XsButtonDirective } from '../../../layouts/coreui/xsbutton-directive/xsbutton.directive';
@@ -138,10 +130,8 @@ import {
     BrowserSoftwareMacosComponent
 } from './browser-software/browser-software-macos/browser-software-macos.component';
 import { PatchstatusIconComponent } from '../../patchstatus/patchstatus-icon/patchstatus-icon.component';
-import { Edge, Network, Node, Options } from 'vis-network';
-import { StatusmapExtendedNode } from '../../statusmaps/statusmap.interface';
-import { DataSet } from 'vis-data';
 import { LayoutService } from '../../../layouts/coreui/layout.service';
+import { ParentChildHostMapComponent } from '../../../components/parent-child-host-map/parent-child-host-map.component';
 
 @Component({
     selector: 'oitc-hosts-browser',
@@ -206,7 +196,7 @@ import { LayoutService } from '../../../layouts/coreui/layout.service';
         BrowserSoftwareWindowsComponent,
         BrowserSoftwareMacosComponent,
         PatchstatusIconComponent,
-        JsonPipe
+        ParentChildHostMapComponent
     ],
     templateUrl: './hosts-browser.component.html',
     styleUrl: './hosts-browser.component.css',
@@ -315,8 +305,6 @@ export class HostsBrowserComponent implements OnInit, OnDestroy {
         this.subscriptions.add(this.HostsService.getHostBrowser(this.id).subscribe((result) => {
             this.cdr.markForCheck();
             this.result = result;
-            // Show parent child relations
-            this.renderVisNetwork();
 
             let priority = Number(result.mergedHost.priority);
             // Sift priority into array index
@@ -621,293 +609,4 @@ export class HostsBrowserComponent implements OnInit, OnDestroy {
     protected readonly Number = Number;
     protected readonly String = String;
     protected readonly Boolean = Boolean;
-    private readonly document = inject(DOCUMENT);
-
-    private renderVisNetwork(): void {
-        if (!this.result) {
-            return;
-        }
-        if (this.result.parentChildRelations.edges === null) {
-            return;
-        }
-        if (this.result.parentChildRelations.nodes === null) {
-            return;
-        }
-
-        const elem = this.document.getElementById('statusmapNetwork');
-        if (!elem) {
-            // Just to make TS happy
-            return;
-        }
-
-        let nodes: DataSet<Node> = new DataSet<StatusmapExtendedNode>();
-        let edges: DataSet<Edge> = new DataSet<Edge>();
-
-        nodes.clear();
-        edges.clear();
-
-        nodes.add(this.result.parentChildRelations.nodes);
-        edges.add(this.result.parentChildRelations.edges);
-
-        const rect = elem.getBoundingClientRect();
-        const offset = {
-            top: rect.top + window.scrollY,
-            left: rect.left + window.scrollX
-        };
-        if (offset.top < 0) {
-            offset.top = 0;
-        }
-
-        const colorUp = '#00C851';
-        const colorDown = '#CC0000';
-        const colorUnreachable = '#727b84';
-        const colorNotMonitored = '#4285F4';
-
-        const labelFontColor: string = this.theme === 'dark' ? "rgba(255, 255, 255, 0.87)" : "rgba(37, 43, 54, 0.95)";
-
-        let options: Options = {
-            autoResize: true,
-            height: String(window.innerHeight - offset.top) + 'px', // I can't stress enough how important the height parameter is!
-            width: "100%",
-            clickToUse: false,
-            groups: {
-                satellite: {
-                    shape: 'ellipse',
-                    margin: {
-                        top: 10,
-                        bottom: 20,
-                        left: 5,
-                        right: 5
-                    }
-                },
-                notMonitored: {
-                    shape: 'dot',
-                    color: colorNotMonitored,
-                    size: 15,
-                    icon: {
-                        face: '"Font Awesome 7 Free Solid"',
-                        code: '\uf070',
-                        color: '#ffffff',
-                        size: 5,
-                        weight: "900", // Font Awesome 5 doesn't work properly unless bold.
-                    }
-                },
-                disabled: {
-                    shape: 'dot',
-                    color: colorNotMonitored,
-                    size: 15,
-                    icon: {
-                        face: '"Font Awesome 7 Free Solid"',
-                        code: '\uf1e6',
-                        color: '#ffffff',
-                        size: 5,
-                        weight: "900", // Font Awesome 5 doesn't work properly unless bold.
-                    }
-                },
-                hostUp: {
-                    shape: 'dot',
-                    color: colorUp,
-                    size: 15,
-                    icon: {
-                        face: '"Font Awesome 7 Free Solid"',
-                        code: '\uf058',
-                        color: '#ffffff',
-                        size: 5,
-                        weight: "900", // Font Awesome 5 doesn't work properly unless bold.
-                    }
-                },
-                hostDown: {
-                    shape: 'dot',
-                    color: colorDown,
-                    size: 15,
-                    icon: {
-                        face: '"Font Awesome 7 Free Solid"',
-                        code: '\uf06a',
-                        color: '#ffffff',
-                        size: 5,
-                        weight: "900", // Font Awesome 5 doesn't work properly unless bold.
-                    }
-                },
-                hostUnreachable: {
-                    shape: 'dot',
-                    color: colorDown,
-                    size: 15,
-                    icon: {
-                        face: '"Font Awesome 7 Free Solid"',
-                        code: '\uf059',
-                        color: '#ffffff',
-                        size: 5,
-                        weight: "900", // Font Awesome 5 doesn't work properly unless bold.
-                    }
-                },
-                isInDowntimeUp: {
-                    shape: 'dot',
-                    color: colorUp,
-                    size: 15,
-                    icon: {
-                        face: '"Font Awesome 7 Free Solid"',
-                        code: '\uf011',
-                        color: '#ffffff',
-                        size: 5,
-                        weight: "900", // Font Awesome 5 doesn't work properly unless bold.
-                    }
-                },
-                isInDowntimeDown: {
-                    shape: 'dot',
-                    color: colorDown,
-                    size: 15,
-                    icon: {
-                        face: '"Font Awesome 7 Free Solid"',
-                        code: '\uf011',
-                        color: '#ffffff',
-                        size: 5,
-                        weight: "900", // Font Awesome 5 doesn't work properly unless bold.
-                    }
-                },
-                isInDowntimeUnreachable: {
-                    shape: 'dot',
-                    color: colorUnreachable,
-                    size: 15,
-                    icon: {
-                        face: '"Font Awesome 7 Free Solid"',
-                        code: '\uf011',
-                        color: '#ffffff',
-                        size: 5,
-                        weight: "900", // Font Awesome 5 doesn't work properly unless bold.
-                    }
-                },
-                isAcknowledgedUp: {
-                    shape: 'dot',
-                    color: colorUp,
-                    size: 15,
-                    icon: {
-                        face: '"Font Awesome 7 Free Solid"',
-                        code: '\uf007',
-                        color: '#ffffff',
-                        size: 5,
-                        weight: "900", // Font Awesome 5 doesn't work properly unless bold.
-                    }
-                },
-                isAcknowledgedDown: {
-                    shape: 'dot',
-                    color: colorDown,
-                    size: 15,
-                    icon: {
-                        face: '"Font Awesome 7 Free Solid"',
-                        code: '\uf007',
-                        color: '#ffffff',
-                        size: 5,
-                        weight: "900", // Font Awesome 5 doesn't work properly unless bold.
-                    }
-                },
-                isAcknowledgedUnreachable: {
-                    shape: 'dot',
-                    color: colorUnreachable,
-                    size: 15,
-                    icon: {
-                        face: '"Font Awesome 7 Free Solid"',
-                        code: '\uf007',
-                        color: '#ffffff',
-                        size: 5,
-                        weight: "900", // Font Awesome 5 doesn't work properly unless bold.
-                    }
-                },
-                isAcknowledgedAndIsInDowntimeUp: {
-                    shape: 'dot',
-                    color: colorUp,
-                    size: 15,
-                    icon: {
-                        face: '"Font Awesome 7 Free Solid"',
-                        code: '\uf0f0',
-                        color: '#ffffff',
-                        size: 5,
-                        weight: "900", // Font Awesome 5 doesn't work properly unless bold.
-                    }
-                },
-                isAcknowledgedAndIsInDowntimeDown: {
-                    shape: 'dot',
-                    color: colorDown,
-                    size: 15,
-                    icon: {
-                        face: '"Font Awesome 7 Free Solid"',
-                        code: '\uf0f0',
-                        color: '#ffffff',
-                        size: 5,
-                        weight: "900", // Font Awesome 5 doesn't work properly unless bold.
-                    }
-                },
-                isAcknowledgedAndIsInDowntimeUnreachable: {
-                    shape: 'dot',
-                    color: colorUnreachable,
-                    size: 15,
-                    icon: {
-                        face: '"Font Awesome 7 Free Solid"',
-                        code: '\uf0f0',
-                        color: '#ffffff',
-                        size: 5,
-                        weight: "900", // Font Awesome 5 doesn't work properly unless bold.
-                    }
-                },
-            },
-            nodes: {
-                borderWidth: 0.5,
-                font: {
-                    color: labelFontColor,
-                    align: "left" // GEHT NICHT
-                }
-            },
-            edges: {
-                width: 1,
-                smooth: false,
-            },
-            interaction: {
-                hover: true,
-                dragNodes: false,
-                keyboard: {
-                    enabled: false
-                },
-                hideEdgesOnDrag: true
-            },
-            layout: {
-                randomSeed: 1000,
-                improvedLayout: false,
-                hierarchical: {
-                    enabled: true,
-                    direction: 'LR',
-                    nodeSpacing: 50,
-                    levelSeparation: 300
-                }
-            },
-            physics: false
-        };
-        const network = new Network(elem, {nodes: nodes, edges: edges}, options);
-        network.fit({
-            animation: {
-                duration: 500,
-                easingFunction: 'linear'
-            }
-        });
-
-        network.on('click', (properties) => {
-            if (properties.nodes.length === 0) {
-                network.fit({
-                    animation: {
-                        duration: 500,
-                        easingFunction: 'linear'
-                    }
-                });
-                return;
-            }
-
-            const nodeId = properties.nodes[0];
-            if (nodeId === 0) {
-                return;
-            }
-
-            // @ts-ignore
-            let selectedNode: StatusmapExtendedNode = nodes.get(nodeId) as StatusmapExtendedNode;
-            // shared $scope with HostSummaryDirective
-        });
-    }
-
 }
