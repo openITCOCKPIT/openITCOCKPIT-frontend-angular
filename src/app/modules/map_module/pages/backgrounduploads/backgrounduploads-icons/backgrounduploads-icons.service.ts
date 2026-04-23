04d57093a3c53675e0058dd172b1bba002162541
ibering <i.bering@avendis.com>
2026-04-23 15:18:35 +0200

    ITC-3759 MapModule - Prevent unauthorized access for non-root users (removed debug output)

--- src/app/modules/map_module/pages/backgrounduploads/backgrounduploads-icons/backgrounduploads-icons.service.ts
+++ src/app/modules/map_module/pages/backgrounduploads/backgrounduploads-icons/backgrounduploads-icons.service.ts
@@ -6,11 +6,8 @@ import { Injectable } from '@angular/core';
 @Injectable({
     providedIn: 'root'
 })
-export class BackgrounduploadsIconsService extends BackgrounduploadsService{
+export class BackgrounduploadsIconsService extends BackgrounduploadsService {
     public override delete(item: DeleteAllItem): Observable<Object> {
-
-        console.log('DELETE HERE !!!');
-
         return this.http.post(`${this.proxyPath}/map_module/backgroundUploads/deleteIcon.json?angular=true`, {
             'filename': item.id
         });
