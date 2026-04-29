2dc98d321b6a2cccaf60ee5bdbedb1266ac7ae85
abergen <a.bergen@avendis.com>
2026-04-29 08:52:30 +0200

    ITC-3765 - added raritan kvm wizard to frontend

---
+++ src/app/modules/raritan_module/pages/wizards/raritan-kvm/raritan-kvm-wizard.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { catchError, map, Observable, of } from 'rxjs';
+import { WizardsService } from '../../../../../pages/wizards/wizards.service';
+import { GenericResponseWrapper, GenericValidationError } from '../../../../../generic-responses';
+import { SNMPWizardPost, WizardGet } from '../../../../../pages/wizards/wizards.interface';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class RaritanKvmWizardService extends WizardsService {
+
+    public fetch(hostId: number): Observable<WizardGet> {
+        return this.http.get<WizardGet>(`${this.proxyPath}/raritan_module/wizards/raritanKvm/${hostId}.json?angular=true`).pipe(
+            map((data: WizardGet): WizardGet => {
+                return data;
+            })
+        );
+    }
+
+    public submit(post: SNMPWizardPost): Observable<GenericResponseWrapper> {
+        return this.http.post<any>(`${this.proxyPath}/raritan_module/wizards/raritanKvm.json?angular=true`, post)
+            .pipe(
+                map(data => {
+                    return {
+                        success: true,
+                        data: null
+                    };
+                }),
+                catchError((error: any) => {
+                    const err = error.error.error as GenericValidationError;
+                    return of({
+                        success: false,
+                        data: err
+                    });
+                })
+            );
+
+    }
+}
